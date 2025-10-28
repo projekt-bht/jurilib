bab6b90c1b1ebc4bba7085f73c00d9b55b0c61c2
s87622 <s87622@bht-berlin.de>
2025-10-28 12:33:47 +0100

    Implementierung von Jest. Frontend und Backendtests sind möglich. Einfache Testbeispiele angelegt, configs angepasst und scripte in der package.json ergänzt

---
+++ src/app/api/test/hallo/route.test.ts
@@ -0,0 +1,6 @@
+
+describe('Backendtest zum testen ', () => {
+  it('sollte true sein', () => {
+    expect(true).toBe(true);
+  });
+});
---
+++ test/styleMock.ts
@@ -0,0 +1,5 @@
+// Jest style mock for handling CSS/SCSS imports in tests
+// Jest cant handle style imports natively, so we provide a mock object
+const styleMock = {};
+export default styleMock;
+
