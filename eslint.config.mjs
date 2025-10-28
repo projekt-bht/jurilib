d18b1a442f5272c6873734e3de8411dbb2cad4b0
dolzycka <dolzyckalian@gmail.com>
2025-10-28 15:40:59 +0100

    Setup with next.js eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
-
+import prettier from "eslint-config-prettier/flat";
+import nextTS from 'eslint-config-next/typescript';
 const eslintConfig = defineConfig([
   ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
+  ...nextTS,
+  prettier, // Override default ignores of eslint-config-next.
   globalIgnores([
     // Default ignores of eslint-config-next:
     ".next/**",
@@ -14,5 +14,4 @@ const eslintConfig = defineConfig([
     "next-env.d.ts",
   ]),
 ]);
-
 export default eslintConfig;
