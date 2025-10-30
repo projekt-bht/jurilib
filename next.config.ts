c57060cdca7b4324909c1a74f550913c42e1abdc
s87622 <s87622@bht-berlin.de>
2025-10-30 15:21:58 +0100

    Anpassung der next.config - angabe der tsconfig, damit next.js automatisch die type erstellen kann

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  // Basic TypeScript configuration
+  typescript: {
+    tsconfigPath: "./tsconfig.json"
+  }
 };
 
 export default nextConfig;
