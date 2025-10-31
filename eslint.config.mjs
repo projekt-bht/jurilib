6e778e23042d90f25b56523579d9a7ee165084fd
dolzycka <dolzyckalian@gmail.com>
2025-10-31 13:05:15 +0100

    Import Sortierung nach Styleguide implementiert

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,13 +1,23 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import prettier from "eslint-config-prettier/flat";
-import nextTS from 'eslint-config-next/typescript';
+import nextTS from "eslint-config-next/typescript";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTS,
   prettier, // Override default ignores of eslint-config-next.
+  {
+    plugins: { "simple-import-sort": simpleImportSort },
+    rules: {
+      // imports und exports sortieren / als warn deklarieren
+      "simple-import-sort/imports": "warn",
+      "simple-import-sort/exports": "warn",
+    },
+  },
   globalIgnores([
     // Default ignores of eslint-config-next:
+    // TODO rausfinden, welche Dateien noch ignoriert werden müssen
     ".next/**",
     "out/**",
     "build/**",
--- test.ts
+++ test.ts
@@ -1,1 +1,14 @@
-let hallo = 200;
+// src/lib/example.ts
+import "@/styles/globals.css";
+
+import fs from "fs";
+import path from "path";
+import React from "react";
+import { z } from "zod";
+
+import type { User } from "../types";
+import { anotherUtil } from "../utils/anotherUtil";
+import { myUtil } from "../utils/myUtil";
+import type { SomeType } from "./localType";
+
+const hallo = 200;
