c238d182f3192039ce82899783f24035a461f552
dolzycka <dolzyckalian@gmail.com>
2025-11-03 14:58:55 +0100

    style(src): linted existing files for consistent setup (closes #7)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,41 @@
-'use client'
-import Link from "next/link"
-import { useEffect, useState } from "react"
-import Test from "../components/TestCompoment/test"
+'use client';
+import Link from 'next/link';
+import { useEffect, useState } from 'react';
+
+import Test from '../components/TestCompoment/test';
 
 type backendResponse = {
-    message: string
-}
+  message: string;
+};
 
 export default function Home() {
+  // const fetchedData: backendResponse =
+  //     await fetch(`${process.env.BACKEND_ROOT}test/hallo`, {
+  //         method: "GET",
+  //         headers: {
+  //             'Content-Type': 'application/json'
+  //         }
+  //     }).then(res => res.json())
 
-    // const fetchedData: backendResponse =
-    //     await fetch(`${process.env.BACKEND_ROOT}test/hallo`, {
-    //         method: "GET",
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     }).then(res => res.json())
+  const [data, setData] = useState<backendResponse>({} as backendResponse);
 
-    const [data, setData] = useState<backendResponse>({} as backendResponse)
+  useEffect(() => {
+    const asyncCall = async () => {
+      const data: backendResponse = await (
+        await fetch(`${process.env.NEXT_PUBLIC_BACKEND_ROOT}test/hallo`)
+      ).json();
+      setData(data);
+    };
+    asyncCall();
+  }, []);
 
-    useEffect(() => {
-        const asyncCall = async () => {
-            const data: backendResponse = await (await fetch(`${process.env.NEXT_PUBLIC_BACKEND_ROOT}test/hallo`)).json()
-            setData(data)
-        }
-        asyncCall()
-    }, [])
-
-    return (
-        <>
-            <Test />
-            <div>
-                <p>
-                    {data.message}
-                </p>
-            </div>
-            <Link href="/test2">Hier lang</Link>
-        </>
-    )
+  return (
+    <>
+      <Test />
+      <div>
+        <p>{data.message}</p>
+      </div>
+      <Link href="/test2">Hier lang</Link>
+    </>
+  );
 }
