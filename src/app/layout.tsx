f64f2e048db3f9fcdface7d3b0cfc14e4b75d330
s87622 <s87622@bht-berlin.de>
2025-11-04 15:19:37 +0100

    Lint fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import "./globals.css";
+import "./globals.css";
 
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
 import { Navbar } from "@/components/Navbar/Navbar";
 
 const geistSans = Geist({
