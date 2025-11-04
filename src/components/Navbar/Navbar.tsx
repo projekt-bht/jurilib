3009f3ecba775e5e76a37e83ea9a47ad31990eaa
s87622 <s87622@bht-berlin.de>
2025-11-04 15:19:26 +0100

    Lint fix and updated some css

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,16 +1,17 @@
 import Link from "next/link"
+
 import { Login } from "../Login/Login"
 
 export function Navbar() {
   return (
-    <nav className="bg-black fixed top-0 left-0 w-full flex items-center justify-between p-5">
+    <nav className="bg-black text-white flex items-center justify-between p-5 mx-auto">
       <div className="flex items-center space-x-2">
-        <Link className="text-lg font-bold" href={"/"}>JuriLib</Link>
+        <Link className="text-lg font-bold" href="/">JuriLib</Link>
       </div>
 
       <div className="flex items-center gap-x-5">
-        <Link href={"/organizations"}>Organisationen</Link>
-        <Login></Login>
+        <Link href="/organizations">Organisationen</Link>
+        <Login />
       </div>
     </nav>
   )
