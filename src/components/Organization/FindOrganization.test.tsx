e8530a395a7c40a61b2bee0b3c5d408b8196feda
parker <141875371+oz28@users.noreply.github.com>
2025-11-05 22:03:29 +0100

    Updated: tests adjusted

--- src/components/Organization/FindOrganization.test.tsx
+++ src/components/Organization/FindOrganization.test.tsx
@@ -1,6 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import { FindOrganization } from './FindOrganization';
 
+
+
+jest.mock('next/navigation', () => ({
+    useRouter: jest.fn(),
+}));
+
 describe('Test LandingPage', () => {
   it('renders the component text', () => {
     render(<FindOrganization />);
