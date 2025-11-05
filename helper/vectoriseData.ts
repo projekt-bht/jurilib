588297ac7ef22783f65c9975683a8033f9179cba
parker <141875371+oz28@users.noreply.github.com>
2025-11-05 10:34:38 +0100

    Updated: API Key nicht public

--- helper/vectoriseData.ts
+++ helper/vectoriseData.ts
@@ -1,8 +1,8 @@
 import OpenAI from "openai";
 
 const openai = new OpenAI({
-  baseURL: "https://openrouter.ai/api/v1",
-  apiKey: "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",
+  baseURL: process.env.OPENAI_BASE_URL || "",
+  apiKey: process.env.OPENAI_API_KEY || "",
   defaultHeaders: {
     'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
     'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
