6a9bc8762bc6abaaf579a5cd6a2f44236ecfb245
parker <141875371+oz28@users.noreply.github.com>
2025-11-05 21:59:38 +0100

    Implemented: searchString to Vector Route

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -1,15 +1,39 @@
 import type { NextRequest} from "next/server";
 import { NextResponse } from "next/server";
+import vectoriseData from "~/helper/vectoriseData";
+import prisma from "@/lib/db";
 
 type SearchRequest ={
-    test: string
+    searchID: string
 }
+
+const similarityOffset = 0.35
 
 export async function POST(req:NextRequest){
     const body= await req.json();
     const manipulatedBody:SearchRequest = body;
 
-    const returnBody = manipulatedBody.test+"... hallo";
+    const query = decodeURIComponent(manipulatedBody.searchID)
+    
+    if(query){
+        const searchInput = await vectoriseData(query)
+        const matches = await prisma.$queryRawUnsafe(`
+        SELECT id, name, "expertiseArea",
+            1 - ("expertiseVector" <=> $1::vector) AS similarity
+        FROM "Organization"
+        WHERE 
+            "expertiseVector" IS NOT NULL
+            AND (1 - ("expertiseVector" <=> $1::vector)) >= $2
+        ORDER BY similarity DESC
+        `, 
+        searchInput,
+        similarityOffset
+        );
 
-    return NextResponse.json(returnBody);
+        console.log(matches)
+        return NextResponse.json(matches, { status: 200 })
+    }
+
+    return NextResponse.json({ message: "Something went wrong" }, { status: 400 })
 }
+
