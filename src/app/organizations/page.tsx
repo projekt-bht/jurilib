7047a86e99d2c6d33532e3a23ccb0737e043e21a
Max Uden <max.uden@t-online.de>
2025-11-09 19:44:08 +0100

    feat(organizations): implement organizations page with loading state and organization listing

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,3 +1,41 @@
-export default function OrganizationsPage() {
-    return <div>Organizations Page</div>;
+import OrganizationCard from "@/components/Organization/OrganizationCard";
+import type { Organization } from "~/generated/prisma/client";
+
+import Loading from "../search/[searchID]/loading";
+
+export default async function OrganizationsPage() {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_ROOT}organization`, { next: { revalidate: 120 } });
+    const organizations: Organization[] = await res.json()
+
+    if (!organizations)
+        return <Loading />
+
+    // to simulate loading time and trigger loading.tsx
+
+    return (
+        <div className="flex flex-col justify-start items-center h-screen pt-3">
+            {organizations.length > 0 ?
+                <>
+                    <p className="text-xl text-gray-800 font-semibold">Organisationsliste</p>
+                    <div className="h-8" />
+                    {
+                        organizations.map((organization) => (
+                            <OrganizationCard
+                                key={"OrganizationCard_" + organization.id}
+                                {...organization}
+                            />
+                        ))
+                    }
+                    <div className="mb-8 text-gray-400 pt-6">
+                        Deine Anfrage wird vertraulich behandelt.
+                    </div>
+                </> :
+                <div className="flex flex-col justify-center items-center h-full text-center gap-y-10">
+                    <p className="text-5xl font-bold text-black">
+                        Leider konnten wir keine passende Organisation finden.
+                    </p>
+                </div>
+            }
+        </div>
+    )
 }   
