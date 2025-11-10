ff3e498d42a60fe01622ea0210cd6c61b0bd8e6d
s87622 <s87622@bht-berlin.de>
2025-11-10 13:14:41 +0100

    refactor(tests): clean up organization route tests for improved readability. Updated import route for vectorizer

--- src/app/api/organization/route.test.ts
+++ src/app/api/organization/route.test.ts
@@ -3,67 +3,65 @@ import { POST, PATCH, GET } from '@/app/api/organization/route';
 import { prisma } from '@/lib/db';
 import { OrganizationCreateInput } from '~/generated/prisma/models';
 
-jest.mock("@helper/vectorizer", () => ({
-  vectorizeExpertiseArea: jest.fn(async () => Array(3072).fill(0.01))
+jest.mock('@/services/server/vectorizer', () => ({
+  vectorizeExpertiseArea: jest.fn(async () => Array(3072).fill(0.01)),
 }));
 
 describe('Organization Routen teset', () => {
+  const baseUrl = `${process.env.NEXT_PUBLIC_BACKEND_ROOT}/organization`;
 
-    const baseUrl = `${process.env.NEXT_PUBLIC_BACKEND_ROOT}/organization`
+  test('POST Organizations', async () => {
+    const organization: OrganizationCreateInput = {
+      name: 'Max Mustermann Kanzlei',
+      description: 'Kanzlei test',
+      email: Math.random() + '@mail.de',
+      type: 'LAW_FIRM',
+      expertiseArea: ['Verkehrsrecht', 'Arbeitsrecht'],
+    };
 
-    test('POST Organizations', async () => {
-        const organization: OrganizationCreateInput = {
-            name: "Max Mustermann Kanzlei",
-            description: "Kanzlei test",
-            email: Math.random() + '@mail.de',
-            type: 'LAW_FIRM',
-            expertiseArea: ["Verkehrsrecht", "Arbeitsrecht"]
-        }
+    const req = new NextRequest(baseUrl, {
+      method: 'POST',
+      body: JSON.stringify(organization),
+    });
 
-        const req = new NextRequest(baseUrl, {
-            method: "POST",
-            body: JSON.stringify(organization)
-        })
+    const res = await POST(req);
+    expect(res.status).toBe(201);
+  });
 
-        const res = await POST(req)
-        expect(res.status).toBe(201)
+  test('PATCH Organizations', async () => {
+    const getReq = new NextRequest(baseUrl);
+    const getRes = await GET(getReq);
+    const getJSON = await getRes.json();
 
-    })
+    const organization: OrganizationCreateInput = {
+      id: getJSON[0].id,
+      name: 'updated',
+      description: 'Kanzlei test',
+      email: Math.random() + '@mail.de',
+      type: 'LAW_FIRM',
+      expertiseArea: ['Verkehrsrecht', 'Arbeitsrecht'],
+    };
 
-    test('PATCH Organizations', async () => {
-        const getReq = new NextRequest(baseUrl)
-        const getRes = await GET(getReq)
-        const getJSON = await getRes.json()
+    const patchReq = new NextRequest(baseUrl, {
+      method: 'PATCH',
+      body: JSON.stringify(organization),
+    });
 
-        const organization: OrganizationCreateInput = {
-            id: getJSON[0].id,
-            name: "updated",
-            description: "Kanzlei test",
-            email: Math.random() + '@mail.de',
-            type: 'LAW_FIRM',
-            expertiseArea: ["Verkehrsrecht", "Arbeitsrecht"]
-        }
+    const res = await PATCH(patchReq);
 
-        const patchReq = new NextRequest(baseUrl, {
-            method: "PATCH",
-            body: JSON.stringify(organization)
-        })
+    const updated = await prisma.organization.findFirst({
+      where: { name: 'updated' },
+    });
 
-        const res = await PATCH(patchReq);
+    expect(updated?.name).toBe('updated');
+    expect(res.status).toBe(200);
+  });
 
-        const updated = await prisma.organization.findFirst({
-            where: { name: "updated" },
-        })
-
-        expect(updated?.name).toBe("updated")
-        expect(res.status).toBe(200)
-    })
-
-    test('GET Organizations', async () => {
-        const req = new NextRequest(baseUrl)
-        const res = await GET(req)
-        const json = await res.json()
-        expect(json.length).not.toBe(0)
-        expect(res.status).toBe(200)
-    })
-})
+  test('GET Organizations', async () => {
+    const req = new NextRequest(baseUrl);
+    const res = await GET(req);
+    const json = await res.json();
+    expect(json.length).not.toBe(0);
+    expect(res.status).toBe(200);
+  });
+});
