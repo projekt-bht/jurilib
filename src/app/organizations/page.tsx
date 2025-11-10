c69f42291b3205c3c265d65bef5e2474f4b8741b
s87622 <s87622@bht-berlin.de>
2025-11-10 12:37:13 +0100

    refactor(organizations): SSG erst nach dem ersten Request, da das Backend beim build noch nicht vorhanden ist

--- src/app/organizations/page.tsx
+++ src/app/organizations/page.tsx
@@ -1,37 +1,49 @@
 import OrganizationCard from '@/components/Organization/OrganizationCard';
 import type { Organization } from '~/generated/prisma/client';
 
-import Loading from '../search/[searchID]/loading';
+// erzwingt SSG
+export const dynamic = 'force-static';
+// ISR mit 1h Cache
+export const revalidate = 3600;
+
+async function getOrganizations(): Promise<Organization[]> {
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_ROOT}organization`, {
+      next: { revalidate },
+    });
+    if (!res.ok) {
+      return [];
+    } else {
+      return (await res.json()) as Organization[];
+    }
+  } catch (error) {
+    console.error('Error fetching organizations:', error);
+    return [];
+  }
+}
 
 export default async function OrganizationsPage() {
-  // by default, data will be fetched when rendered data has changed
-  const res = await fetch(`http://localhost:3000/api/organization`);
-  const organizations: Organization[] = await res.json();
-
-  if (!organizations) return <Loading />;
-
-  // to simulate loading time and trigger loading.tsx
+  const organizations: Organization[] = await getOrganizations();
 
   return (
-    organizations && (
-      <div className="flex flex-col justify-start items-center h-screen pt-3">
-        {organizations.length > 0 ? (
-          <>
-            <p className="text-xl text-gray-800 font-semibold">Organisationsliste</p>
-            <div className="h-8" />
-            {organizations.map((organization) => (
-              <OrganizationCard key={'OrganizationCard_' + organization.id} {...organization} />
-            ))}
-            <div className="mb-8 text-gray-400 pt-6">Deine Anfrage wird vertraulich behandelt.</div>
-          </>
-        ) : (
-          <div className="flex flex-col justify-center items-center h-full text-center gap-y-10">
-            <p className="text-5xl font-bold text-black">
-              Leider konnten wir keine passende Organisation finden.
-            </p>
-          </div>
-        )}
-      </div>
-    )
+    <div className="flex flex-col justify-start items-center h-screen pt-3">
+      {organizations.length > 0 ? (
+        <>
+          <p className="text-xl text-gray-800 font-semibold">Organisationsliste</p>
+          <div className="h-8" />
+          {organizations.map((o) => (
+            <OrganizationCard key={'OrganizationCard_' + o.id} {...o} />
+          ))}
+          <div className="mb-8 text-gray-400 pt-6">Deine Anfrage wird vertraulich behandelt.</div>
+        </>
+      ) : (
+        <div className="flex flex-col justify-center items-center h-full text-center gap-y-10">
+          <p className="text-5xl font-bold text-black">
+            Leider konnten wir keine passende Organisation finden.
+          </p>
+          <p className="text-gray-500">Hinweis: Beim Build war die API evtl. nicht erreichbar.</p>
+        </div>
+      )}
+    </div>
   );
 }
