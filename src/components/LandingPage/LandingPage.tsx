31a549b5a8521c30e19256f99670c4c06dd90013
s87622 <s87622@bht-berlin.de>
2025-11-18 12:32:12 +0100

    refactor(LandingPage, FindOrganization): linting

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,26 +1,16 @@
-import { FindOrganization } from "../Organization/FindOrganization";
+import { FindOrganization } from '../Organization/FindOrganization';
 
 export function LandingPage() {
-
   return (
-    <>
     <div className="bg-white min-h-screen flex items-center justify-center">
       <div className="text-center max-w-2xl w-full">
-        
-        <h1 className="text-black text-xl font-bold mb-2">
-          Beschreibe dein Problem
-        </h1>
-        <p className="text-black mb-6">
-          Teile uns dein rechtliches Anliegen mit
-        </p>
-        
-        <FindOrganization/>
+        <h1 className="text-black text-xl font-bold mb-2">Beschreibe dein Problem</h1>
+        <p className="text-black mb-6">Teile uns dein rechtliches Anliegen mit</p>
 
-        <p className="text-gray-400 text-sm mt-10">
-          Deine Anfrage wird vertraulich behandelt
-        </p>
+        <FindOrganization />
+
+        <p className="text-gray-400 text-sm mt-10">Deine Anfrage wird vertraulich behandelt</p>
       </div>
     </div>
-    </>
-  )
+  );
 }
