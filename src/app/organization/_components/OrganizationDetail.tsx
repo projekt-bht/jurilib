f5414b864e5bc7e9600e4bc4821c5744ab5c51b4
s87622 <s87622@bht-berlin.de>
2025-11-18 12:51:48 +0100

    feat(OrganizationDetail): refactor organisation detail page and move code to a new file. Delete styling file

---
+++ src/app/organization/_components/OrganizationDetail.tsx
@@ -0,0 +1,68 @@
+import { Clock, Mail, MapPin, Phone, Scale } from 'lucide-react';
+
+import type { Organization } from '~/generated/prisma/client';
+
+import OrganizationCalendar from './OrganizationCalendar';
+
+export default function OrganizationDetail(organization: Organization) {
+  return (
+    <div
+      id={`orgaDetailPage_${organization.id}`}
+      className="flex flex-col justify-start items-center w-full px-4 py-6"
+    >
+      <div className="outline-1 p-6 rounded-lg w-full max-w-5xl outline-gray-300 shadow-md">
+        <p className="text-2xl font-bold mb-4">{organization.name}</p>
+        <p> {organization.description}</p>
+        <div className="grid grid-cols-2 gap-x-40 gap-y-16 pt-10 justify-items-start">
+          <div className="col-span-1">
+            <div className="flex items-center gap-2 mb-2">
+              <Scale color="grey" size={25} />
+              <p className="flex items-center gap-2 text-xl font-bold text-gray-700">Fachgebiete</p>
+            </div>
+            <p>{organization.expertiseArea}</p>
+          </div>
+          <div className="col-span-1">
+            <div className="flex items-center gap-2 mb-2">
+              <MapPin color="grey" size={25} />
+              <p className="flex items-center gap-2 text-xl font-bold text-gray-700">Adresse</p>
+            </div>
+            <p>{organization.address}</p>
+          </div>
+          <div className="col-span-1">
+            <div className="flex items-center gap-2 mb-2">
+              <Phone color="grey" size={25} />
+              <p className="flex items-center gap-2 text-xl font-bold text-gray-700">Telefon</p>
+            </div>
+            <p>{organization.phone}</p>
+          </div>
+          <div className="col-span-1">
+            <div className="flex items-center gap-2 mb-2">
+              <Mail color="grey" size={25} />
+              <p className="flex items-center gap-2 text-xl font-bold text-gray-700">E-Mail</p>
+            </div>
+            <p>{organization.email}</p>
+          </div>
+          <div className="col-span-1">
+            <div className="flex items-center gap-2 mb-2">
+              <Clock color="grey" size={25} />
+              <p className="flex items-center gap-2 text-xl font-bold text-gray-700">
+                Öffnungszeiten
+              </p>
+            </div>
+            <p>Hier könnten Kontaktdaten stehen.</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="outline-1 p-6 rounded-lg w-full max-w-5xl outline-gray-300 shadow-md mt-6">
+        <p className="text-base font-bold mb-4">Termin vereinbaren</p>
+        <div className="grid grid-cols-2 gap-x-40 gap-y-16 pt-10 justify-items-start">
+          <div className="col-span-1 justify-center items-left flex flex-col gap-4">
+            <p className="text-base text-gray-600">Wählen Sie ein Datum</p>
+            <OrganizationCalendar />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
