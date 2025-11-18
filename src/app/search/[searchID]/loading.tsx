7be5b545b2ddbcffc1fec549d4e40560a9be54ab
s87622 <s87622@bht-berlin.de>
2025-11-18 12:31:09 +0100

    refactor(Loading): simplify loading component by using LoadingComponent

--- src/app/search/[searchID]/loading.tsx
+++ src/app/search/[searchID]/loading.tsx
@@ -1,20 +1,5 @@
-import { Spinner } from "@/components/ui/spinner";
-export default function Loading() {
-    return (
-        <div className="flex flex-col justify-between items-center h-screen">
-            <div className="flex flex-col justify-center items-center grow gap-6">
-                <Spinner className="size-18 text-gray-700" />
-                <div className="text-lg pt-5 text-black">
-                    Warten Sie, während wir eine passende Organisation finden
-                </div>
-                <div className="text-base text-gray-500">
-                    Dies dauert nur einen Moment...
-                </div>
-            </div>
-            <div className="mb-8 text-gray-400">
-                Deine Anfrage wird vertraulich behandelt.
-            </div>
-        </div>
+import LoadingComponent from '@/components/Loading/LoadingComponent';
 
-    );
+export default function Loading() {
+  return <LoadingComponent message="Wir suchen nach passenden Organisationen für dich..." />;
 }
