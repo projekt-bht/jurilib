f4817042a3dff70eb65f1855deb42837a85e174b
dolzycka <dolzyckalian@gmail.com>
2025-11-18 11:14:32 +0100

    rebase(search): added both search algorithims to seprate files to be rebased later

--- src/app/api/search/routeFullTextSearch copy.ts
+++ src/app/api/search/routeFullTextSearch copy.ts
@@ -1,51 +1,5 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-<<<<<<< HEAD
-import prisma from '@/lib/db';
-import { vectorizeSearch } from '@/services/server/vectorizer';
-
-type SearchRequest = {
-  searchID: string;
-};
-
-const similarityOffset = 0.4;
-const threshold = 0.12;
-
-export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const manipulatedBody: SearchRequest = body;
-
-  const query = decodeURIComponent(manipulatedBody.searchID);
-
-  if (query) {
-    const searchInput = await vectorizeSearch(query);
-    const matches = await prisma.$queryRawUnsafe<
-      { id: string; name: string; expertiseArea: string; similarity: number }[]
-    >(
-      `
-        SELECT id, name, "expertiseArea",
-            1 - ("expertiseVector" <=> $1::vector) AS similarity
-        FROM "Organization"
-        WHERE 
-            "expertiseVector" IS NOT NULL
-            AND (1 - ("expertiseVector" <=> $1::vector)) >= $2
-        ORDER BY similarity DESC
-        `,
-      searchInput,
-      similarityOffset
-    );
-
-    const highestSimilarity = matches[0]?.similarity;
-    const filteredMatches = matches.filter(
-      (match) => match.similarity >= highestSimilarity - threshold
-    );
-    console.log(filteredMatches);
-    //console.log(matches)
-    return NextResponse.json(filteredMatches, { status: 200 });
-  }
-
-  return NextResponse.json({ message: 'Something went wrong' }, { status: 400 });
-=======
 
 import { prisma } from '@/lib/db';
 
@@ -131,7 +85,6 @@ export async function POST(req: NextRequest) {
 
     return defaultErrorResponse(error);
   }
->>>>>>> 8defdb7 (feat(serch): implemented first version of full text serch for field of law (#36))
 }
 
 // Helper response functions
