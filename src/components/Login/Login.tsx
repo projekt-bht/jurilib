7ac41c5e997b1af2ccae84c3975c39f61adf8ba5
s87622 <s87622@bht-berlin.de>
2025-11-19 12:21:37 +0100

    refactor: linting + prettier

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -2,10 +2,13 @@
 
 export function Login() {
   //Login Logik
-
+  //
   return (
-    <button onClick={() => console.log("Login")} className="bg-white text-black p-2 pr-3 pl-3 rounded-full">
+    <button
+      onClick={() => console.log('Login')}
+      className="bg-white text-black p-2 pr-3 pl-3 rounded-full"
+    >
       Einloggen
     </button>
-  )
+  );
 }
