45fab39314dd67fe47dba9dbfc55b821ed2664ea
s87622 <s87622@bht-berlin.de>
2025-11-19 14:24:36 +0100

    feat(api): enhance organization GET endpoint error handling and added organization endpoint DELETE

--- src/app/api/organization/[organizationID]/route.ts
+++ src/app/api/organization/[organizationID]/route.ts
@@ -8,6 +8,28 @@ export async function GET(
   { params }: { params: Promise<{ organizationID: string }> }
 ) {
   const { organizationID } = await params;
-  const matchedOrg = await db.organization.findUnique({ where: { id: organizationID } });
-  return NextResponse.json(matchedOrg);
+  try {
+    const matchedOrg = await db.organization.findUnique({ where: { id: organizationID } });
+    if (!matchedOrg) {
+      return NextResponse.json({ message: 'Organization not found' }, { status: 404 });
+    }
+    return NextResponse.json(matchedOrg);
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
 }
+
+export async function DELETE(
+  req: NextRequest,
+  { params }: { params: Promise<{ organizationID: string }> }
+) {
+  const { organizationID } = await params;
+  try {
+    await db.organization.delete({ where: { id: organizationID } });
+  } catch (error) {
+    return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+  }
+  return NextResponse.json({ message: 'Organization successfully deleted' }, { status: 200 });
+}
+
+
