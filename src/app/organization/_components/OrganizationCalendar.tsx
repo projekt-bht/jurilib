8a78de4e37943c92b357bbf04053cde807b4aa41
s87622 <s87622@bht-berlin.de>
2025-11-19 12:20:30 +0100

    refactor(OrganizationCalander): linting

--- src/app/organization/_components/OrganizationCalendar.tsx
+++ src/app/organization/_components/OrganizationCalendar.tsx
@@ -1,22 +1,21 @@
-"use client"
+'use client';
 
-import { de } from "date-fns/locale/de"
-import * as React from "react"
+import { de } from 'date-fns/locale/de';
+import * as React from 'react';
 
-import { Calendar } from "@/components/ui/calendar"
-
+import { Calendar } from '@/components/ui/calendar';
 
 export default function OrganizationCalendar() {
-    const [date, setDate] = React.useState<Date | undefined>(new Date())
+  const [date, setDate] = React.useState<Date | undefined>(new Date());
 
-    return (
-        <Calendar
-            locale={de}
-            mode="single"
-            selected={date}
-            onSelect={setDate}
-            className="rounded-md border shadow-sm"
-            captionLayout="dropdown"
-        />
-    )
+  return (
+    <Calendar
+      locale={de}
+      mode="single"
+      selected={date}
+      onSelect={setDate}
+      className="rounded-md border shadow-sm"
+      captionLayout="dropdown"
+    />
+  );
 }
