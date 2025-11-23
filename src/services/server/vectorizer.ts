1c482240fd43a589427de02b6ad56fd73aca8633
Max Uden <max.uden@t-online.de>
2025-11-23 17:07:18 +0100

    style(prettier): styling stuff...

--- src/services/server/vectorizer.ts
+++ src/services/server/vectorizer.ts
@@ -1,15 +1,16 @@
-import OpenAI from "openai";
+/* eslint-disable no-console */
+import OpenAI from 'openai';
 
 const openai = new OpenAI({
-  baseURL: process.env.OPENAI_BASE_URL || "",
-  apiKey: process.env.OPENAI_API_KEY || "",
+  baseURL: process.env.OPENAI_BASE_URL ?? '',
+  apiKey: process.env.OPENAI_API_KEY ?? '',
   defaultHeaders: {
     'HTTP-Referer': '<YOUR_SITE_URL>', // Optional. Site URL for rankings on openrouter.ai.
     'X-Title': '<YOUR_SITE_NAME>', // Optional. Site title for rankings on openrouter.ai.
   },
 });
 
-export async function vectorizeSearch(query : string){
+export async function vectorizeSearch(query: string) {
   const expansionPrompt = `
     Versuche bitte diesen Text zu juristschen rechtlichen Fachgebieten zuzuordnen, bzw. wo das hingehören könnte.
     BEISPIEL: Strafrecht, Verkehrsrecht, Mietercht, Zivilrecht, Arbeitsrecht und so weiter ...
@@ -19,35 +20,35 @@ export async function vectorizeSearch(query : string){
     `;
 
   /*
-    System role: Allows you to specify the way the model answers questions. Classic example: “You are a helpful assistant.”
-    User role: Equivalent to the queries made by the user.
-    Assistant role: are the model’s responses (based on the user messages)
-  */
+      System role: Allows you to specify the way the model answers questions. Classic example: “You are a helpful assistant.”
+      User role: Equivalent to the queries made by the user.
+      Assistant role: are the model’s responses (based on the user messages)
+    */
   const expansion = await openai.chat.completions.create({
-    model: "gpt-4o-mini",
+    model: 'gpt-4o-mini',
     messages: [
-      { role: 'system', content: "Du bist ein juristisch versiertes Modell" },
-      { role: "user", content: expansionPrompt },
+      { role: 'system', content: 'Du bist ein juristisch versiertes Modell' },
+      { role: 'user', content: expansionPrompt },
     ],
   });
 
-  const expandedQuery = expansion?.choices[0].message.content?.trim() || query;
-  console.log("Original:", query);
-  console.log("Expanded:", expandedQuery);
+  const expandedQuery = expansion?.choices[0].message.content?.trim() ?? query;
+  console.log('Original:', query);
+  console.log('Expanded:', expandedQuery);
 
   const embeddingResponse = await openai.embeddings.create({
-    model: "openai/text-embedding-3-large",
+    model: 'openai/text-embedding-3-large',
     input: expandedQuery,
   });
 
-  return embeddingResponse.data[0].embedding
+  return embeddingResponse.data[0].embedding;
 }
 
-export async function vectorizeExpertiseArea(query : string){
-  console.log(query);
+export async function vectorizeExpertiseArea(query: string) {
+  //   console.log(query);
   const embeddingResponse = await openai.embeddings.create({
-    model: "openai/text-embedding-3-large",
+    model: 'openai/text-embedding-3-large',
     input: query,
   });
-  return embeddingResponse.data[0].embedding
+  return embeddingResponse.data[0].embedding;
 }
