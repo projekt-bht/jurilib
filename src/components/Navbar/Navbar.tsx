d5d5ab950e88af365435c6b1acf8fdda2aedf030
s87622 <s87622@bht-berlin.de>
2025-11-25 16:44:24 +0100

    feat(Navbar): added logo to navbar and refactor logo display and link structure

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,15 +1,24 @@
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { Login } from '@/components/Login/Login';
+import scale_logo from '~/public/scale_logo.svg';
 
 export function Navbar() {
   return (
     <nav className="bg-background text-foreground flex items-center justify-between p-5 mx-auto border border-border">
-      <div className="flex items-center space-x-2">
-        <Link className="text-2xl font-bold" href="/">
-          JuriLib
-        </Link>
-      </div>
+      <Link href="/" className="flex items-center space-x-2">
+        <div className="bg-foreground p-2 rounded-lg">
+          <Image
+            src={scale_logo}
+            alt="JuriLib Logo"
+            width={40}
+            height={40}
+            className="brightness-0 invert gray"
+          />
+        </div>
+        <span className="text-2xl font-bold">JuriLib</span>
+      </Link>
 
       <div className="flex items-center gap-x-5">
         <Link href="/organization">Organisationen</Link>
