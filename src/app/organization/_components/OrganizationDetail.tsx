bd537197e3c3c4bd98dcf845764b00fc11de4d2a
s87622 <s87622@bht-berlin.de>
2025-11-27 11:00:22 +0100

    feat(OrganizationDetailPage): update layout and remove OrganizationCalendar component

--- src/app/organization/_components/OrganizationDetail.tsx
+++ src/app/organization/_components/OrganizationDetail.tsx
@@ -2,8 +2,6 @@ import { Clock, Mail, MapPin, Phone, Scale } from 'lucide-react';
 
 import type { Organization } from '~/generated/prisma/client';
 
-import OrganizationCalendar from './OrganizationCalendar';
-
 export default function OrganizationDetail(organization: Organization) {
   return (
     <div
@@ -11,9 +9,16 @@ export default function OrganizationDetail(organization: Organization) {
       className="flex flex-col justify-start items-center w-full px-4 py-6"
     >
       <div className="bg-background outline-1 p-6 rounded-lg w-full max-w-5xl outline-border shadow-md">
-        <p className="text-2xl font-bold mb-4">{organization.name}</p>
+        <div className="flex items-center mb-4">
+          <p className="text-4xl font-bold pr-4">{organization.name}</p>
+          <div className="text-base inline-block px-2 py-1 rounded-xl border border-accent-amber font-semibold bg-accent-amber-light text-accent-amber">
+            €€€
+          </div>
+        </div>
+
         <p> {organization.description}</p>
-        <div className="grid grid-cols-2 gap-x-40 gap-y-16 pt-10 justify-items-start">
+
+        <div className="grid grid-cols-2 gap-x-10 gap-y-5 pt-8 justify-items-start">
           <div className="col-span-1">
             <div className="flex items-center gap-2 mb-2">
               <Scale className="text-foreground" size={25} />
@@ -23,6 +28,7 @@ export default function OrganizationDetail(organization: Organization) {
             </div>
             <p>{organization.expertiseArea}</p>
           </div>
+
           <div className="col-span-1">
             <div className="flex items-center gap-2 mb-2">
               <MapPin className="text-foreground" size={25} />
@@ -30,6 +36,7 @@ export default function OrganizationDetail(organization: Organization) {
             </div>
             <p>{organization.address}</p>
           </div>
+
           <div className="col-span-1">
             <div className="flex items-center gap-2 mb-2">
               <Phone className="text-foreground" size={25} />
@@ -37,6 +44,7 @@ export default function OrganizationDetail(organization: Organization) {
             </div>
             <p>{organization.phone}</p>
           </div>
+
           <div className="col-span-1">
             <div className="flex items-center gap-2 mb-2">
               <Mail className="text-foreground" size={25} />
@@ -44,6 +52,7 @@ export default function OrganizationDetail(organization: Organization) {
             </div>
             <p>{organization.email}</p>
           </div>
+
           <div className="col-span-1">
             <div className="flex items-center gap-2 mb-2">
               <Clock className="text-foreground" size={25} />
@@ -52,16 +61,6 @@ export default function OrganizationDetail(organization: Organization) {
               </p>
             </div>
             <p>Hier könnten Kontaktdaten stehen.</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="bg-background outline-1 p-6 rounded-lg w-full max-w-5xl outline-border shadow-md mt-6">
-        <p className="text-base font-bold mb-4">Termin vereinbaren</p>
-        <div className="grid grid-cols-2 gap-x-40 gap-y-16 pt-10 justify-items-start">
-          <div className="col-span-1 justify-center items-left flex flex-col gap-4">
-            <p className="text-base text-gray-600">Wählen Sie ein Datum</p>
-            <OrganizationCalendar />
           </div>
         </div>
       </div>
