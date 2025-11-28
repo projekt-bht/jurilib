1a547ca02826c7550a74a50f9b7d6d7eac009617
s87622 <s87622@bht-berlin.de>
2025-11-28 11:21:42 +0100

    feat(OrganizationDetailPage): refactor layout and introduce profileItem helper component

--- src/app/organization/_components/OrganizationDetail.tsx
+++ src/app/organization/_components/OrganizationDetail.tsx
@@ -1,6 +1,24 @@
-import { Clock, Mail, MapPin, Phone, Scale } from 'lucide-react';
+import { Separator } from '@radix-ui/react-separator';
+import { Globe, Mail, MapPin, Phone } from 'lucide-react';
 
 import type { Organization } from '~/generated/prisma/client';
+
+// Helper component for profile items
+function profileItem(
+  Icon: React.ComponentType<{ className?: string; size?: number }>,
+  title: string,
+  content: string | null
+) {
+  return (
+    <div className="col-span-1">
+      <div className="flex items-center gap-2 mb-2">
+        <Icon className="text-foreground" size={25} />
+        <p className="flex items-center gap-2 text-xl font-bold text-foreground">{title}</p>
+      </div>
+      <p>{content ?? 'Keine Angabe.'}</p>
+    </div>
+  );
+}
 
 export default function OrganizationDetail(organization: Organization) {
   return (
@@ -15,53 +33,20 @@ export default function OrganizationDetail(organization: Organization) {
             €€€
           </div>
         </div>
+        <div className="text-sm inline-block px-2 py-1 rounded-xl  border border-accent-amber font-semibold bg-accent-amber-light text-accent-amber mb-4">
+          <p>{organization.expertiseArea}</p>
+        </div>
+        <div className="mb-4 pt-2 text-foreground text-lg">
+          <p>{organization.description}</p>
+        </div>
 
-        <p> {organization.description}</p>
+        <Separator className="my-6 h-px bg-border w-full" />
 
-        <div className="grid grid-cols-2 gap-x-10 gap-y-5 pt-8 justify-items-start">
-          <div className="col-span-1">
-            <div className="flex items-center gap-2 mb-2">
-              <Scale className="text-foreground" size={25} />
-              <p className="flex items-center gap-2 text-xl font-bold text-foreground">
-                Fachgebiete
-              </p>
-            </div>
-            <p>{organization.expertiseArea}</p>
-          </div>
-
-          <div className="col-span-1">
-            <div className="flex items-center gap-2 mb-2">
-              <MapPin className="text-foreground" size={25} />
-              <p className="flex items-center gap-2 text-xl font-bold text-foreground">Adresse</p>
-            </div>
-            <p>{organization.address}</p>
-          </div>
-
-          <div className="col-span-1">
-            <div className="flex items-center gap-2 mb-2">
-              <Phone className="text-foreground" size={25} />
-              <p className="flex items-center gap-2 text-xl font-bold text-foreground">Telefon</p>
-            </div>
-            <p>{organization.phone}</p>
-          </div>
-
-          <div className="col-span-1">
-            <div className="flex items-center gap-2 mb-2">
-              <Mail className="text-foreground" size={25} />
-              <p className="flex items-center gap-2 text-xl font-bold text-foreground">E-Mail</p>
-            </div>
-            <p>{organization.email}</p>
-          </div>
-
-          <div className="col-span-1">
-            <div className="flex items-center gap-2 mb-2">
-              <Clock className="text-foreground" size={25} />
-              <p className="flex items-center gap-2 text-xl font-bold text-foreground">
-                Öffnungszeiten
-              </p>
-            </div>
-            <p>Hier könnten Kontaktdaten stehen.</p>
-          </div>
+        <div className="grid grid-cols-2 gap-x-10 gap-y-5 justify-items-start">
+          {profileItem(Globe, 'Webseite', organization.website)}
+          {profileItem(Phone, 'Telefon', organization.phone)}
+          {profileItem(MapPin, 'Adresse', organization.address)}
+          {profileItem(Mail, 'E-Mail', organization.email)}
         </div>
       </div>
     </div>
