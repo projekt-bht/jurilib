759534afc2c64f787e4aa6355742989c27f39687
s87622 <s87622@bht-berlin.de>
2025-12-03 18:26:22 +0100

    refactor: update OrganizationDetail component structure and improve profile info handling

--- src/app/organization/_components/OrganizationDetail.tsx
+++ src/app/organization/_components/OrganizationDetail.tsx
@@ -1,16 +1,22 @@
+'use client';
 import { Separator } from '@radix-ui/react-separator';
 import { Globe, Mail, MapPin, Phone } from 'lucide-react';
 
 import type { Areas, Organization } from '~/generated/prisma/client';
 
-import formatPriceCategory from './organizationHelper';
+import { PricingInfo } from './OrganizationPricing';
 
-// Helper component for profile items
-function createProfileInfoItem(
-  Icon: React.ComponentType<{ className?: string; size?: number }>,
-  title: string,
-  content: string | null
-) {
+// Function to create and formate the Profile Info Items with icon, title and content
+// Used to create items like Website, Phone, Address, Email
+function ProfileInfoItem({
+  Icon,
+  title,
+  content,
+}: {
+  Icon: React.ComponentType<{ className?: string; size?: number }>;
+  title: string;
+  content: string | null;
+}) {
   return (
     <div className="col-span-1">
       <div className="flex items-center gap-2 mb-2">
@@ -22,13 +28,14 @@ function createProfileInfoItem(
   );
 }
 
-function createProfileExpertiseAreaItem(areas: Areas[]) {
+// Function to create and format the Expertise Area items
+function ExpertiseAreaItem({ areas }: { areas: Areas[] }) {
   return areas.map((area) => (
     <div
       key={area}
       className="text-sm inline-block px-2 py-1 rounded-xl  border border-accent-amber font-semibold bg-accent-amber-light text-accent-amber mb-2 mr-2"
     >
-      <p>{area}</p>
+      {area}
     </div>
   ));
 }
@@ -37,25 +44,28 @@ export default function OrganizationDetail(organization: Organization) {
   return (
     <div
       id={`OrganizationDetailPage_${organization.id}`}
-      className="flex flex-col justify-start items-center w-full px-4 py-6"
+      className="flex flex-col justify-start items-center w-full px-10 py-8"
     >
       <div className="bg-background outline-1 p-6 rounded-lg w-full max-w-5xl outline-border shadow-md">
         <div className="flex items-center mb-4">
           <p className="text-4xl font-bold pr-4">{organization.name}</p>
-          {formatPriceCategory(organization.priceCategory)}
         </div>
-        {createProfileExpertiseAreaItem(organization.expertiseArea)}
+
+        <ExpertiseAreaItem areas={organization.expertiseArea} />
+
         <div className="mb-4 pt-2 text-foreground text-lg">
           <p>{organization.shortDescription}</p>
         </div>
 
+        <PricingInfo priceCategory={organization.priceCategory} />
+
         <Separator className="my-6 h-px bg-border w-full" />
 
         <div className="grid grid-cols-2 gap-x-10 gap-y-5 justify-items-start">
-          {createProfileInfoItem(Globe, 'Webseite', organization.website)}
-          {createProfileInfoItem(Phone, 'Telefon', organization.phone)}
-          {createProfileInfoItem(MapPin, 'Adresse', organization.address)}
-          {createProfileInfoItem(Mail, 'E-Mail', organization.email)}
+          <ProfileInfoItem Icon={Globe} title="Webseite" content={organization.website} />
+          <ProfileInfoItem Icon={Phone} title="Telefon" content={organization.phone} />
+          <ProfileInfoItem Icon={MapPin} title="Adresse" content={organization.address} />
+          <ProfileInfoItem Icon={Mail} title="E-Mail" content={organization.email} />
         </div>
       </div>
 
