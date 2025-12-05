34605a6702bb3cd555f779130d977713cdc4416b
s87622 <s87622@bht-berlin.de>
2025-12-05 13:08:47 +0100

    feat(LandingPage): update layout and remove redundant text

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -4,17 +4,13 @@ import { ProblemSearchField } from './ProblemSearchField';
 
 export function LandingPage() {
   return (
-    <div className="bg-card min-h-screen flex items-center justify-center mt-10">
-      <div className="text-center w-full">
+    <div className="bg-card min-h-screen flex items-center justify-center">
+      <div className="text-center w-full mt-10">
         <h1 className="text-foreground text-5xl font-bold mb-2">Beschreibe dein Problem</h1>
         <p className="text-foreground mb-3 py-6 pt-4">Teile uns dein rechtliches Anliegen mit</p>
         <div className="min-w-sm mx-auto max-w-lg">
           <ProblemSearchField />
         </div>
-
-        <p className="text-muted-foreground text-sm m-4">
-          Deine Anfrage wird vertraulich behandelt
-        </p>
         <HowItWorks />
         <OurOffer />
       </div>
