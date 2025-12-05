c2f206d672354129bb1b0ef02532f4a253953032
s87622 <s87622@bht-berlin.de>
2025-12-05 01:55:12 +0100

    feat(organization): enhance Profile component with new organization type badge and layout improvements

--- src/app/organization/_components/Profile.tsx
+++ src/app/organization/_components/Profile.tsx
@@ -1,6 +1,6 @@
 'use client';
 import { Separator } from '@radix-ui/react-separator';
-import { Globe, Info, Mail, MapPin, Phone, Star, Users } from 'lucide-react';
+import { Building, Building2, Globe, Info, Mail, MapPin, Phone, Star, Users } from 'lucide-react';
 
 import type { Areas, Organization } from '~/generated/prisma/client';
 
@@ -34,12 +34,37 @@ function ExpertiseAreaItem({ areas }: { areas: Areas[] }) {
   return areas.map((area) => (
     <div
       key={area}
-      className="text-sm inline-block px-2 py-1 rounded-xl  border border-accent-amber font-semibold bg-accent-amber-light text-accent-amber mb-2 mr-2"
+      className="text-sm inline-block px-3 py-1 rounded-xl  border border-accent-amber font-semibold bg-accent-amber-light text-accent-amber shadow-sm shadow-accent-amber-light"
     >
       {area}
     </div>
   ));
 }
+
+function OrganisationTypeBadge({ type }: { type: string }) {
+  let displayType = '';
+  let icon = null;
+  switch (type) {
+    case 'LAW_FIRM':
+      displayType = 'Kanzlei';
+      icon = <Building className="w-4 h-4 text-accent-gray" />;
+      break;
+    case 'ASSOCIATION':
+      displayType = 'Verein';
+      icon = <Building2 className="w-4 h-4 text-accent-gray" />;
+      break;
+    default:
+      displayType = 'Keine Angabe';
+      icon = <Info className="w-4 h-4 text-accent-gray" />;
+  }
+  return (
+    <span className="px-3 py-1 rounded-full text-sm font-semibold bg-accent-blue-soft border border-accent-gray-light text-foreground inline-flex items-center gap-1">
+      {icon}
+      {displayType}
+    </span>
+  );
+}
+
 // export function Profile(organization: Organization, employees: Employee[]) {
 export function Profile(organization: Organization) {
   return (
@@ -48,28 +73,31 @@ export function Profile(organization: Organization) {
       className="flex flex-col justify-start items-start w-full px-10 py-8"
     >
       {/* Profile Info Section */}
-      <div className="bg-background outline-1 p-6 rounded-lg w-full max-w-5xl outline-border shadow-md">
+      <div className="bg-background border p-6 rounded-lg w-full max-w-5xl border-border shadow-md">
         <div className="flex flex-col lg:flex-row  items-start gap-8">
           {/* Organization logo */}
-          <div className="w-24 h-24 rounded-full bg-linear-to-br from-blue-500 to-purple-600 flex items-center justify-center text-white text-3xl font-bold shadow-lg">
+          <div className="w-24 h-24 rounded-full bg-linear-to-br from-blue-500 to-purple-600 flex items-center justify-center text-white text-3xl font-bold shadow-lg shrink-0">
             {organization.name.charAt(0)}
           </div>
-          <div className="flex flex-col gap-2">
-            <p className="text-4xl font-bold">{organization.name}</p>
+          <div className="flex flex-col gap-2 flex-1">
+            <h2 className="text-4xl font-bold">{organization.name}</h2>
+            <div className="pb-2">
+              <OrganisationTypeBadge type={organization.type} />
+            </div>
+
+            <div className="mb-4text-foreground text-lg pb-2">{organization.shortDescription}</div>
+            <div>
+              <PricingInfo priceCategory={organization.priceCategory} />
+            </div>
             <div className="flex flex-wrap items-start gap-2">
               <ExpertiseAreaItem areas={organization.expertiseArea} />
-            </div>
-            <div className="mb-4 pt-2 text-foreground text-lg">
-              <p>{organization.shortDescription}</p>
             </div>
           </div>
         </div>
 
-        <PricingInfo priceCategory={organization.priceCategory} />
-
         <Separator className="my-6 h-px bg-border w-full" />
 
-        <div className="grid grid-cols-2 gap-x-10 gap-y-5 justify-items-start">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-x-10 gap-y-5">
           <ProfileInfoItem Icon={Globe} title="Webseite" content={organization.website} />
           <ProfileInfoItem Icon={Phone} title="Telefon" content={organization.phone} />
           <ProfileInfoItem Icon={MapPin} title="Adresse" content={organization.address} />
@@ -78,7 +106,7 @@ export function Profile(organization: Organization) {
       </div>
 
       {/* Description Section */}
-      <div className="bg-background outline-1 p-6 mt-6 rounded-lg w-full max-w-5xl outline-border shadow-md">
+      <div className="bg-background border p-6 mt-6 rounded-lg w-full max-w-5xl border-border shadow-md">
         <h2 className="text-2xl font-bold mb-4 flex items-center gap-1">
           <Info className="w-6 h-6 text-blue-600 inline-block mr-2" />
           Über uns
@@ -88,7 +116,7 @@ export function Profile(organization: Organization) {
 
       {/* Employees Section */}
       {/* Hier noch prüfen, ob Employees vorhanden sind */}
-      <div className="bg-background outline-1 p-6 mt-6 rounded-lg w-full max-w-5xl outline-border shadow-md">
+      <div className="bg-background border p-6 mt-6 rounded-lg w-full max-w-5xl border-border shadow-md">
         <h2 className="text-2xl font-bold mb-4 flex items-center gap-1">
           <Users className="w-6 h-6 text-blue-600 inline-block mr-2" />
           Unser Team
@@ -140,7 +168,7 @@ export function Profile(organization: Organization) {
 
       {/* Bewertungen */}
       {/* DEMO! Kann ausgetauscht werden, wenn Bewertungen da sind. Aktuell nur zur Veranschaulichung */}
-      <div className="bg-background outline-1 p-6 mt-6 rounded-lg w-full max-w-5xl outline-border shadow-md">
+      <div className="bg-background border p-6 mt-6 rounded-lg w-full max-w-5xl border-border shadow-md">
         <h2 className="text-2xl font-bold mb-4 flex items-center gap-1">
           <Star className="w-6 h-6 text-accent-amber fill-accent-amber-light inline-block mr-2" />
           Kundenbewertungen
