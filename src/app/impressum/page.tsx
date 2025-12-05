60c0d36372cb6557840fa92a4c86486df946541f
Max Uden <max.uden@t-online.de>
2025-12-05 13:44:29 +0100

    feat(impressum): added seperate impressum page. YES, it's called Impressum!

---
+++ src/app/impressum/page.tsx
@@ -0,0 +1,10 @@
+export default function Impressum() {
+  return (
+    <div className="min-h-screen bg-background flex flex-col">
+      <div className="max-w-6xl mx-auto text-center">
+        <h1 className="text-5xl font-bold text-foreground mb-6 text-balance">Impressum</h1>
+        <p>Hier stehen bald rechtliche Informationen.</p>
+      </div>
+    </div>
+  );
+}
