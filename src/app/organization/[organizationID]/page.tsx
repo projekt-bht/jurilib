259c34eee6305f1e513d7eba80fce570d981c34f
s87622 <s87622@bht-berlin.de>
2025-12-06 15:18:39 +0100

    feat(organization): refactor OrganizationDetailPage to use centralized fetch function and improve error handling

--- src/app/organization/[organizationID]/page.tsx
+++ src/app/organization/[organizationID]/page.tsx
@@ -1,41 +1,50 @@
+import { notFound } from 'next/navigation';
+
 import { Profile } from '@/app/organization/_components/Profile';
 import type { Organization } from '~/generated/prisma/client';
 
-// import OrganizationCalendar from '../_components/OrganizationCalendar';
+async function fetchBackendData(endpoint: string, organizationID: string): Promise<Response> {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_ROOT}${endpoint}/${organizationID}`, {
+    cache: 'no-store',
+  });
+
+  // Rufe NotFound-Seite auf, wenn Endpoint oder Inhalt nicht gefunden wurde
+  if (res.status === 404) {
+    notFound();
+  }
+
+  // Treten andere Fehler auf, werfe einen Fehler, der vom Error Boundary in der error.tsx behandelt wird
+  if (!res.ok) {
+    throw new Error(`Failed to fetch organization: ${res.statusText}`);
+  }
+  return res;
+}
 
 export default async function OrganizationDetailPage({
   params,
 }: {
   params: { organizationID: string };
 }) {
-  const parameters = await params;
-  const organizationID = parameters.organizationID;
-  const resOrga = await fetch(
-    `${process.env.NEXT_PUBLIC_BACKEND_ROOT}organization/${organizationID}`
-  );
+  const { organizationID } = await params;
+
+  // Parse die Organisationsdaten
+  // Auftretende Fehler werden ebenfalls vom Error Boundary behandelt
+
+  const OrgaResponse = await fetchBackendData('organization', organizationID);
+  const organization: Organization = await OrgaResponse.json();
 
   // Wenn Employee Endpunkt fertig ist, wieder entkommentieren
-  // const resEmployee = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_ROOT}employee/organization/${organizationID}`);
+  // const resEmployee = await fetchBackendData('employee', organizationID);
   // const employees: Employee[] = await resEmployee.json();
 
-  // TODO: Error Handling einbauen
-
-  const org: Organization = await resOrga.json();
-
-  return org ? (
+  return (
     <div className="bg-card grid grid-cols-1 lg:grid-cols-3 gap-5">
       {/* Left Column - Profile Info */}
       <div className="lg:col-span-2 space-y-8">
-        <Profile {...org} />
+        <Profile {...organization} />
       </div>
       {/* Right Column - Booking Section */}
       <div className="lg:col-span-1 space-y-8">{/*<OrganizationCalendar />*/}</div>
-    </div>
-  ) : (
-    <div className="flex flex-col justify-center items-center h-full text-center gap-y-10">
-      <p className="text-5xl font-bold text-foreground">
-        Leider konnten wir keine passende Organisation finden.
-      </p>
     </div>
   );
 }
