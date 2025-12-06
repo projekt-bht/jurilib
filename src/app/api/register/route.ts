b80131709dc256410032608575ad40ab056d0910
parker <141875371+oz28@users.noreply.github.com>
2025-12-06 21:19:36 +0100

    feat(register): register Route

---
+++ src/app/api/register/route.ts
@@ -0,0 +1,33 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+import { createAccount } from '../account/services';
+import { createUser } from '../user/services';
+
+export async function POST(req: NextRequest) {
+  try {
+    if (!req.headers.get('content-type')?.includes('application/json')) {
+      return NextResponse.json({ message: 'Invalid content type' }, { status: 415 });
+    }
+
+    const body = await req.json();
+
+    const createdAccount = await createAccount(body.account);
+
+    switch (createdAccount.role) {
+      case 'USER':
+        const createdUser = await createUser(body.entity, createdAccount.id);
+        return NextResponse.json(createdUser, { status: 201 });
+      case 'EMPLOYEE':
+        //TODO
+        break;
+      default:
+        break;
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Creation failed: ' + (error as Error).message },
+      { status: 400 }
+    );
+  }
+}
