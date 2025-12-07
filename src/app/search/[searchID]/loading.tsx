709974fc19ab4dac66c8ae214757fbf091cb05dc
s87622 <s87622@bht-berlin.de>
2025-12-07 20:36:16 +0100

    refactor(search): removed complete route

--- src/app/search/[searchID]/loading.tsx
+++
@@ -1,5 +0,0 @@
-import LoadingComponent from '@/components/Loading/LoadingComponent';
-
-export default function Loading() {
-  return <LoadingComponent message="Wir suchen nach passenden Organisationen für dich..." />;
-}
