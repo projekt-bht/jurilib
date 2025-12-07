7b6c8aea00fa82f28fc70ff9db4729a1c4f47210
dolzycka <dolzyckalian@gmail.com>
2025-12-07 22:39:57 +0100

    feat(appt): added update / PATCH appt

--- src/app/api/appointment/[employeeID]/[appointmentID]/route.ts
+++ src/app/api/appointment/[employeeID]/[appointmentID]/route.ts
@@ -1,12 +1,77 @@
-import type { NextRequest } from 'next/server';
+import { type NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
 
-// PUT /api/appointment/[employeeID]/[appointmentID]
+import { AppointmentStatus } from '~/generated/prisma/enums';
+
+import { handleValidationError, headerSchema } from '../route';
+import { updateAppointment } from './service';
+
+/**
+ * Validate parameter employeeID and appointmentID
+ */
+const paramsSchema = z.object({
+  employeeID: z.string().min(1, 'Employee ID is required'),
+  appointmentID: z.string().min(1, 'Appointment ID is required'),
+});
+
+/**
+ * Validate the attributes that can be updated in an appointment
+ * attributes not included here cannot be updated
+ * dateTimeEnd is not included, as it is calculated based on dateTimeStart and duration
+ */
+export const appointmentUpdateSchema = z.object({
+  serviceID: z.string().min(1, 'Service ID is required').optional(),
+  duration: z.number().min(1, 'Duration must be at least 1 minute').optional(),
+  status: z.enum(AppointmentStatus, { message: 'Invalid status value' }).optional(),
+  location: z.string().optional(),
+  meetingLink: z.string().optional(), // may require URL validation later
+  dateTimeStart: z
+    .string()
+    .refine((dateStr) => !isNaN(Date.parse(dateStr)), {
+      message: 'Invalid date format',
+    })
+    .optional(),
+  notes: z.string().optional(),
+});
+
+// GET /api/appointment/:employeeID/:appointmentID
+// Retrieve a specific appointment (to be implemented)
+export async function GET(_req: NextRequest) {}
+
+// PATCH /api/appointment/:employeeID/:appointmentID
 // Update an appointment (to be implemented)
-export async function PUT(_req: NextRequest) {
-  // TODO: To be implemented
+export async function PATCH(
+  req: NextRequest,
+  { params }: { params: Promise<{ employeeID: string; appointmentID: string }> }
+) {
+  try {
+    //validate header
+    headerSchema.parse(req.headers);
+    // validate params
+    const { employeeID, appointmentID } = await params;
+    paramsSchema.parse({ employeeID, appointmentID });
+    // validate body
+    const body = appointmentUpdateSchema.parse(await req.json());
+    if (!body || Object.keys(body).length === 0) {
+      return NextResponse.json({ message: 'Update data is required' }, { status: 400 });
+    }
+
+    // update appointment
+    const updatedAppointment = await updateAppointment(employeeID, appointmentID, body);
+    return NextResponse.json(updatedAppointment, { status: 200 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return handleValidationError(error);
+    } else {
+      return NextResponse.json(
+        { message: 'Update failed: ' + (error as Error).message },
+        { status: 400 }
+      );
+    }
+  }
 }
 
-// DELETE /api/appointment/[employeeID]/[appointmentID]
+// DELETE /api/appointment/:employeeID/:appointmentID
 // Delete an appointment (to be implemented)
 export async function DELETE(_req: NextRequest) {
   // TODO: To be implemented
--- src/app/api/appointment/[employeeID]/[appointmentID]/service.ts
+++ src/app/api/appointment/[employeeID]/[appointmentID]/service.ts
@@ -1,8 +1,80 @@
+import { ValidationError } from '@/error/validationErrors';
+import prisma from '@/lib/db';
+import type { Appointment } from '~/generated/prisma/client';
+import type { AppointmentStatus } from '~/generated/prisma/client';
+
+import { validateNotOverlapping } from '../services';
+
+type ZodUpdateAppointment = {
+  serviceID?: string;
+  duration?: number;
+  status?: AppointmentStatus;
+  location?: string;
+  meetingLink?: string; // may require URL validation later
+  dateTimeStart?: string;
+  notes?: string;
+};
+
 // Read a specific appointment by ID
 // TODO: implement
 
 // Update an appointment by ID
-// TODO: implement
+export async function updateAppointment(
+  employeeID: string,
+  appointmentID: string,
+  appointment: ZodUpdateAppointment
+): Promise<Appointment> {
+  try {
+    const existingAppointment = await validateReference(employeeID, appointmentID);
+
+    // Determine whether dateTimeEnd needs to be recalculated based on updated fields
+    // only need to validate overlapping if dateTimeStart or duration is being updated
+    if (appointment.dateTimeStart || appointment.duration) {
+      const startTime = new Date(appointment.dateTimeStart ?? existingAppointment.dateTimeStart);
+      const endTime = new Date(
+        startTime.getTime() + (appointment.duration ?? existingAppointment.duration) * 60000
+      );
+      if (
+        existingAppointment.dateTimeStart !== startTime ||
+        existingAppointment.dateTimeEnd !== endTime
+      ) {
+        // Recalculate dateTimeEnd based on new dateTimeStart and duration
+        await validateNotOverlapping(startTime, endTime, employeeID, appointmentID);
+      }
+    }
+
+    // update appointment
+    const updatedAppointment = await prisma.appointment.update({
+      where: { id: appointmentID },
+      data: {
+        serviceId: appointment.serviceID ?? existingAppointment.serviceId ?? undefined,
+        duration: appointment.duration ?? existingAppointment.duration,
+        status: appointment.status ?? existingAppointment.status,
+        location: appointment.location ?? existingAppointment.location ?? undefined,
+        meetingLink: appointment.meetingLink ?? existingAppointment.meetingLink ?? undefined,
+        dateTimeStart: appointment.dateTimeStart ?? existingAppointment.dateTimeStart,
+        notes: appointment.notes ?? existingAppointment.notes ?? undefined,
+      },
+    });
+    return updatedAppointment;
+  } catch (error) {
+    throw new Error('Database update failed: ' + (error as Error).message);
+  }
+}
 
 // Delete an appointment by ID
 // TODO: implement
+
+export async function validateReference(paramEmployeeID: string, paramAppointmentID: string) {
+  //check if employee exists
+  if (!(await prisma.employee.findUnique({ where: { id: paramEmployeeID } }))) {
+    throw new ValidationError('notFound', 'employeeId', paramEmployeeID);
+  }
+  // check if appointment exists
+  const appointment = await prisma.appointment.findUnique({ where: { id: paramAppointmentID } });
+  if (!appointment) {
+    throw new ValidationError('notFound', 'appointmentId', paramAppointmentID);
+  }
+
+  return appointment;
+}
