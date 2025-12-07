aa8980f8d6b94757657bfa7abf8be07d7b5ce280
parker <141875371+oz28@users.noreply.github.com>
2025-12-07 15:41:14 +0100

    feat(service):using Resource to avoid sending password to FE

--- src/app/api/account/[accountID]/services.ts
+++ src/app/api/account/[accountID]/services.ts
@@ -1,10 +1,11 @@
 import bcrypt from 'bcryptjs';
 
-import prisma from '@/lib/db';
-import type { Account } from '~/generated/prisma/client';
 import { ValidationError } from '@/error/validationErrors';
+import prisma from '@/lib/db';
+import type { AccountResource } from '@/services/Resources';
+import type { Account } from '~/generated/prisma/client';
 
-export const readAccount = async (accountID: string): Promise<Account> => {
+export const readAccount = async (accountID: string): Promise<AccountResource> => {
   try {
     const account: Account | null = await prisma.account.findUnique({
       where: { id: accountID },
@@ -12,13 +13,23 @@ export const readAccount = async (accountID: string): Promise<Account> => {
     if (!account) {
       throw new ValidationError('notFound', 'accounts', accountID);
     }
-    return account;
+
+    const accountRes = {
+      id: account.id,
+      email: account.email,
+      role: account.role,
+    };
+
+    return accountRes;
   } catch (error) {
     throw new Error('Database query failed: ' + (error as Error).message);
   }
 };
 
-export const updateAccount = async (account: Account, accountId: string): Promise<Account> => {
+export const updateAccount = async (
+  account: Account,
+  accountId: string
+): Promise<AccountResource> => {
   try {
     const existingAccount = await prisma.account.findUnique({ where: { id: accountId } });
     if (!existingAccount) {
@@ -34,7 +45,13 @@ export const updateAccount = async (account: Account, accountId: string): Promis
       },
     });
 
-    return updatedAccount;
+    const accountRes = {
+      id: updatedAccount.id,
+      email: updatedAccount.email,
+      role: updatedAccount.role,
+    };
+
+    return accountRes;
   } catch (error) {
     throw new Error('Database update failed' + (error as Error).message);
   }
