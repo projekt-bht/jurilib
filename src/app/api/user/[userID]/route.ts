27637579d65483062e740563808e6b506e6fd95b
parker <141875371+oz28@users.noreply.github.com>
2025-12-07 15:41:59 +0100

    feat(zod): validation for User

--- src/app/api/user/[userID]/route.ts
+++ src/app/api/user/[userID]/route.ts
@@ -2,6 +2,18 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 import { deleteUser, readUser, updateUser } from './services';
+import z from 'zod';
+import { Role } from '~/generated/prisma/enums';
+import { User } from '~/generated/prisma/browser';
+
+const UpdateSchema = z.object({
+  id: z.string().min(36),
+  name: z.string().optional(),
+  phone: z.string().optional(),
+  address: z.string().optional(),
+  password: z.string().min(6).optional(),
+  role: z.enum(Role).optional().optional(),
+});
 
 export async function GET(_req: NextRequest, { params }: { params: Promise<{ userID: string }> }) {
   try {
@@ -22,19 +34,23 @@ export async function PATCH(req: NextRequest, { params }: { params: Promise<{ us
     if (!req.headers.get('content-type')?.includes('application/json')) {
       return NextResponse.json({ message: 'Invalid content type' }, { status: 415 });
     }
-    const body = await req.json();
-
-    if (!body || Object.keys(body).length === 0) {
-      return NextResponse.json({ message: 'Update data is required' }, { status: 400 });
-    }
     const { userID } = await params;
     if (!userID) {
       return NextResponse.json({ message: 'User ID is required' }, { status: 400 });
     }
 
-    const updatedUser = await updateUser(body, userID);
+    const body = await req.json();
+    const validatedBody = UpdateSchema.parse(body);
+
+    const updatedUser = await updateUser(validatedBody as User, userID);
     return NextResponse.json(updatedUser, { status: 200 });
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      return NextResponse.json(
+        { message: 'Validation Problem: ' + (error as Error).message },
+        { status: 400 }
+      );
+    }
     return NextResponse.json(
       { message: 'Failed to update User: ' + (error as Error).message },
       { status: 400 }
