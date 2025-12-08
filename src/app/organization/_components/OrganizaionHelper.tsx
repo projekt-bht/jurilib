81af5151ffbd3d37c6d58f43d515c832f69d73ce
s87622 <s87622@bht-berlin.de>
2025-12-08 23:00:50 +0100

    feat(OrganizaionHelper): update imports and enhance OrganisationTypeBadge with shadow effect

--- src/app/organization/_components/OrganizaionHelper.tsx
+++ src/app/organization/_components/OrganizaionHelper.tsx
@@ -1,6 +1,7 @@
 import { Building, Building2, Info } from 'lucide-react';
 
-import { OrganizationType } from '~/generated/prisma/client';
+import type { Areas } from '~/generated/prisma/browser';
+import { OrganizationType } from '~/generated/prisma/browser';
 
 // Function to create and format the Organisation Type Badge
 export function OrganisationTypeBadge({ type }: { type: OrganizationType }) {
@@ -20,9 +21,24 @@ export function OrganisationTypeBadge({ type }: { type: OrganizationType }) {
       icon = <Info className="w-4 h-4 text-accent-gray" />;
   }
   return (
-    <span className="px-3 py-1 rounded-full text-sm font-semibold bg-accent-blue-soft border border-accent-gray-light text-foreground inline-flex items-center gap-1">
+    <span className="px-3 py-1 rounded-full text-sm font-semibold bg-accent-blue-soft border border-accent-gray-light text-foreground inline-flex items-center gap-1 shadow-sm">
       {icon}
       {displayType}
     </span>
+  );
+}
+// Function to create and format the Expertise Area items to badges
+export function ExpertiseAreaItem({ areas }: { areas: Areas[] }) {
+  return (
+    <>
+      {areas.map((area) => (
+        <div
+          key={area}
+          className="text-sm inline-block px-3 py-1 rounded-xl font-semibold bg-accent-blue-soft border border-accent-gray-light text-foreground shadow-sm"
+        >
+          {area}
+        </div>
+      ))}
+    </>
   );
 }
