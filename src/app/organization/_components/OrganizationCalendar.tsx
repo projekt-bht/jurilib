cb66bae938648f145d2a4593a7ca69e0840f61d9
s87619 <s87619@bht-berlin.de>
2025-12-09 23:34:47 +0100

    style(CalendarWithTime): update button styles for improved visibility and accessibility

--- src/app/organization/_components/OrganizationCalendar.tsx
+++ src/app/organization/_components/OrganizationCalendar.tsx
@@ -251,7 +251,7 @@ export function CalendarWithTime({ onChange }: CalendarWithTimeProps) {
 
           <div className="space-y-2 pt-2">
             <Button
-              className="w-full bg-accent-blue text-accent-white hover:bg-accent-blue-light border border-accent-blue hover:border-accent-blue-light disabled:opacity-60"
+              className="w-full bg-accent-black text-accent-white hover:bg-accent-black-light border border-accent-black hover:border-accent-black-light disabled:opacity-60 text-lg font-semibold"
               disabled={!selectedDate || !selectedTime || isBooking}
               onClick={confirmBooking}
             >
