961ba8e185ece8e3cf45a1cbc669667b14a9a4cb
dolzycka <dolzyckalian@gmail.com>
2025-12-11 19:29:41 +0100

    feat(registration-transaction): removed POST for account since accounts are created through a transaction in registration

--- src/app/api/account/route.ts
+++ src/app/api/account/route.ts
@@ -1,41 +1,7 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-import * as z from 'zod';
 
-import { Role } from '~/generated/prisma/enums';
-
-import { createAccount, readAccounts } from './services';
-
-const CreateSchema = z.object({
-  email: z.string(),
-  password: z.string().min(6),
-  role: z.enum(Role),
-});
-
-export async function POST(req: NextRequest) {
-  try {
-    if (!req.headers.get('content-type')?.includes('application/json')) {
-      return NextResponse.json({ message: 'Invalid content type' }, { status: 415 });
-    }
-
-    const body = await req.json();
-    const data = CreateSchema.parse(body);
-    const createdAccount = await createAccount(data);
-
-    return NextResponse.json(createdAccount, { status: 201 });
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return NextResponse.json(
-        { message: 'Validation Problem: ' + (error as Error).message },
-        { status: 400 }
-      );
-    }
-    return NextResponse.json(
-      { message: 'Creation failed: ' + (error as Error).message },
-      { status: 400 }
-    );
-  }
-}
+import { readAccounts } from './services';
 
 export async function GET(_req: NextRequest) {
   try {
