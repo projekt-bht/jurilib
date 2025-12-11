5a2438b9cbd3e2e5eb35d7b1c535f14b69c950da
s87622 <s87622@bht-berlin.de>
2025-12-11 23:55:16 +0100

    refactor(Navbar): make navbar fixed and full-width for improved layout

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -7,7 +7,7 @@ import scale_logo from '~/public/scale_logo.svg';
 
 export function Navbar() {
   return (
-    <nav className="bg-background text-foreground flex items-center justify-between p-5 mx-auto border border-border">
+    <nav className="bg-background text-foreground flex items-center justify-between p-5 mx-auto border border-border fixed w-full z-50">
       <Link href="/" className="flex items-center space-x-2">
         <div className="bg-foreground p-2 rounded-lg">
           <Image
