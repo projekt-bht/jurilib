1b82d7eec8f47bf8e1810efa4f394820b95043bb
dolzycka <dolzyckalian@gmail.com>
2025-12-11 12:15:18 +0100

    feat(service-search): added try-catch to serch route

--- src/app/api/search/route.ts
+++ src/app/api/search/route.ts
@@ -8,15 +8,22 @@ type SearchRequest = {
 };
 
 export async function POST(req: NextRequest) {
-  const body = await req.json();
-  const manipulatedBody: SearchRequest = body;
+  try {
+    const body = await req.json();
+    const manipulatedBody: SearchRequest = body;
 
-  const query = decodeURIComponent(manipulatedBody.searchID);
-  const filteredMatches = await createSearch(query);
+    const query = decodeURIComponent(manipulatedBody.searchID);
+    const filteredMatches = await createSearch(query);
 
-  if (filteredMatches) {
-    return NextResponse.json(filteredMatches, { status: 200 });
-  } else {
-    return NextResponse.json({ message: 'Something went wrong' }, { status: 400 });
+    if (filteredMatches) {
+      return NextResponse.json(filteredMatches, { status: 200 });
+    } else {
+      return NextResponse.json({ message: 'Something went wrong' }, { status: 400 });
+    }
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Something went wrong: ' + (error as Error).message },
+      { status: 400 }
+    );
   }
 }
