1dade339241f6114e70c6a86b8acda2047e3fcdf
dolzycka <dolzyckalian@gmail.com>
2025-12-22 20:05:24 +0100

    feat(transaktion): further debugging of tests

--- src/app/api/employee/[employeeID]/route.test.ts
+++ src/app/api/employee/[employeeID]/route.test.ts
@@ -1,11 +1,13 @@
 import type { Employee } from '~/generated/prisma/client';
-import { EmployeeCreateInput } from '~/generated/prisma/models';
+import { EmployeeCreateInput, OrganizationCreateInput } from '~/generated/prisma/models';
+import { createOrganization } from '../../organization/services';
 
 // Alle Imports per await:
 const { NextRequest } = await import('next/server');
 const { prisma } = await import('@/lib/db');
 
 // Dynamisch die API-Funktionen importieren
+const { POST: orgPOST } = await import('@/app/api/organization/route');
 const { GET, PATCH, DELETE } = await import('@/app/api/employee/[employeeID]/route');
 
 // !!!! Viele Tests können aktuell nicht durchgeführt werden, da es keine Account-Erstellung für Employees gibt !!!!
@@ -13,6 +15,56 @@ const { GET, PATCH, DELETE } = await import('@/app/api/employee/[employeeID]/rou
 describe('Employee Endpoint /employee/[employeeID] testen', () => {
   const baseUrl = `${process.env.NEXT_PUBLIC_BACKEND_ROOT}/employee/[employeeID]`;
   let cEmployee: Employee;
+
+  test('Create Account and Employee', async () => {
+    // Create Organization first
+    const organization: OrganizationCreateInput = {
+      name: 'Test Org for Employee',
+      description: 'Org Description',
+      shortDescription: 'Org Short Desc',
+      email: 'orgemail_test' + Math.random() + '@mail.de',
+      type: 'LAW_FIRM',
+      priceCategory: 'MEDIUM',
+      expertiseArea: ['Verkehrsrecht', 'Arbeitsrecht'],
+    };
+
+    const reqOrga = new NextRequest(baseUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(organization),
+    });
+
+    const resOrga = await orgPOST(reqOrga);
+    console.log('TEST - Created Organization Response Status:', resOrga.status);
+    const orgaJsonResponse = await resOrga.json();
+    console.log('TEST - Created Organization response message:', orgaJsonResponse.message);
+    expect(resOrga.status).toBe(201);
+
+    const createdOrg = await resOrga.json();
+
+    // Create both account and employee through registration route
+    const registerInput = {
+      email: 'EMPLOYEE_TEST_' + Math.random() + '@mail.de',
+      password: '123456',
+      role: 'EMPLOYEE',
+      name: 'Peter Mustermann',
+      organizationId: createdOrg.id,
+      expertiseArea: ['Arbeitsrecht', 'Familienrecht'],
+    };
+
+    const reqRegister = new NextRequest(baseUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(registerInput),
+    });
+
+    const resRegister = await POST(reqRegister);
+    expect(resRegister.status).toBe(201);
+
+    const createdEmployee = await resRegister.json();
+    cEmployee = createdEmployee;
+    expect(createdEmployee.email).toBe(registerInput.email);
+  });
 
   // Currently, there is no Account creation functionality for Employees
 
