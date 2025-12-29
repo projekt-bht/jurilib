ce105f91a3db6982421f10ff709658ecbf4be42a
s87622 <s87622@bht-berlin.de>
2025-12-29 21:55:39 +0100

    feat(search): update organization fields in createSearch function and adjust expertise area handling

--- src/app/api/search/services.ts
+++ src/app/api/search/services.ts
@@ -1,33 +1,54 @@
 import prisma from '@/lib/db';
 import { vectorizeSearch } from '@/services/server/vectorizer';
-import type { Areas, OrganizationType, PriceCategory } from '~/generated/prisma/client';
+import type {
+  Accessibility,
+  Area,
+  OrganizationType,
+  PriceCategory,
+} from '~/generated/prisma/client';
 import type { Organization } from '~/generated/prisma/client';
 
 const similarityOffset = 0.8;
 const threshold = 0.12;
 
 export async function createSearch(query: string) {
-    const searchInput = await vectorizeSearch(query);
+  const searchInput = await vectorizeSearch(query);
 
-    const matches = await prisma.$queryRaw<
-      Array<{
-        name: string;
-        id: string;
-        description: string | null;
-        shortDescription: string;
-        email: string;
-        phone: string | null;
-        address: string | null;
-        website: string | null;
-        expertiseArea: Areas[];
-        expertiseVector: string;
-        type: OrganizationType;
-        priceCategory: PriceCategory;
-        createdAt: Date;
-        updatedAt: Date;
-        similarity: number;
-      }>
-    >`
+  const matches = await prisma.$queryRaw<
+    Array<{
+      /* Organization fields */
+      id: string;
+      name: string;
+      description: string;
+      shortDescription: string;
+      email: string;
+      phone: string | null;
+      website: string | null;
+      imageUrl: string | null;
+      accessibility: Accessibility[];
+      expertiseAreas: Area[];
+
+      type: OrganizationType;
+      priceCategory: PriceCategory;
+
+      country: string;
+      city: string;
+      zipCode: string;
+      street: string;
+      houseNumber: string;
+
+      averageRating: number;
+      numberOfRatings: number;
+
+      createdAt: Date;
+      updatedAt: Date;
+
+      expertiseVector: string;
+
+      /* Internal use for similarity calculation */
+      similarity: number;
+    }>
+  >`
       SELECT
       *, 
       1 - ("expertiseVector" <=> ${searchInput}::vector) AS similarity
@@ -38,28 +59,29 @@ export async function createSearch(query: string) {
       ORDER BY similarity DESC
       `;
 
-    const highestSimilarity = matches[0]?.similarity;
-    const filteredMatches = matches.filter(
-      (match) => match.similarity >= highestSimilarity - threshold
-    );
+  const highestSimilarity = matches[0]?.similarity;
+  const filteredMatches = matches.filter(
+    (match) => match.similarity >= highestSimilarity - threshold
+  );
 
-    // Transformiere expertiseArea zu einem String und entferne similarity
-    const transformedMatches: Organization[] = filteredMatches.map((match) => {
-      const { similarity, expertiseVector, ...rest } = match;
-      return {
-        ...rest,
-        expertiseArea: Array.isArray(match.expertiseArea)
-          ? match.expertiseArea // Bereits Array: belasse es
-          : (String(match.expertiseArea)
-              .replace(/{|}/g, '') // Entferne {}
-              .split(',') // Splitte durch ,
-              .map((s: string) => s.trim()) // Trimme
-              .filter((s: string) => s) as Areas[]), // Entferne leere Strings
-      };
-    });
+  // Transformiere expertiseArea zu einem String und entferne similarity
+  const transformedMatches: Organization[] = filteredMatches.map((match) => {
+    //eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { similarity, expertiseVector, ...rest } = match;
+    return {
+      ...rest,
+      expertiseAreas: Array.isArray(match.expertiseAreas)
+        ? match.expertiseAreas // Bereits Array: belasse es
+        : (String(match.expertiseAreas)
+            .replace(/{|}/g, '') // Entferne {}
+            .split(',') // Splitte durch ,
+            .map((s: string) => s.trim()) // Trimme
+            .filter((s: string) => s) as Area[]), // Entferne leere Strings
+    };
+  });
 
-    // eslint-disable-next-line no-console
-    console.log(transformedMatches);
+  // eslint-disable-next-line no-console
+  console.log(transformedMatches);
 
-    return transformedMatches;
+  return transformedMatches;
 }
