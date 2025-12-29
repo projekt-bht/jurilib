1b7e6507afe06f6c48e642acba747789bb7fbe1a
s87622 <s87622@bht-berlin.de>
2025-12-29 21:34:53 +0100

    feat(employee): update employee schema

--- src/app/api/employee/[employeeID]/route.ts
+++ src/app/api/employee/[employeeID]/route.ts
@@ -3,17 +3,26 @@ import { NextResponse } from 'next/server';
 import z from 'zod';
 
 import type { Employee } from '~/generated/prisma/client';
-import { Areas } from '~/generated/prisma/enums';
+import { Area, Gender, Language, Pronoun } from '~/generated/prisma/enums';
 
 import { deleteEmployee, readEmployeeByEmployeeID, updateEmployee } from './services';
 
 const UpdateSchemaEmployee = z.object({
   id: z.string().min(36),
   //organizationId: z.string().optional(), // organizationId should not be updated, maybe later
-  name: z.string().optional(),
+  firstName: z.string().min(1).optional(),
+  lastName: z.string().min(1).optional(),
+  pronoun: z.enum(Pronoun).optional(),
+  pronounText: z.string().optional(),
+  gender: z.enum(Gender).optional(),
+  genderText: z.string().optional(),
+  email: z.email().optional(),
   phone: z.string().optional(),
   position: z.string().optional(),
-  expertiseAreas: z.array(z.enum(Areas)).optional(),
+  description: z.string().optional(),
+  cv: z.array(z.string()).optional(),
+  languages: z.array(z.enum(Language)).optional(),
+  expertiseAreas: z.array(z.enum(Area)).optional(),
 });
 
 export async function GET(
