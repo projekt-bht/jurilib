7cdf1c0b4435b7b6a1b13901f065232f43b9b4ba
s87622 <s87622@bht-berlin.de>
2026-01-09 12:32:09 +0100

    refactor(authentication): replace 'role' with 'type' in account object

--- src/components/Authentication/Authentication.tsx
+++ src/components/Authentication/Authentication.tsx
@@ -43,7 +43,7 @@ export function Authentication() {
           account: {
             email: data.email.toString(),
             password: data.password.toString(),
-            role: 'USER',
+            type: 'USER',
           },
           entity: {
             name: data.name.toString(),
