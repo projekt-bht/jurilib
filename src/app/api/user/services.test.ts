e2800d8c4afc447b4cf351e541370ef6c2e88828
s87622 <s87622@bht-berlin.de>
2026-01-12 14:24:43 +0100

    refactor(tests): correct pronoun enum value from 'HE_HIM' to 'er_ihm' in user tests

--- src/app/api/user/services.test.ts
+++ src/app/api/user/services.test.ts
@@ -19,7 +19,7 @@ describe('User testen', () => {
       lastname: 'pan',
       birthdate: new Date('1990-01-01'),
       gender: Gender.Mann,
-      pronoun: Pronoun.HE_HIM,
+      pronoun: Pronoun.er_ihm,
       phone: '0123456789',
 
       country: 'Germany',
