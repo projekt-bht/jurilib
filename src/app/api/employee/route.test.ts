529e68b59d441b0802304fd78f8a351962e7203f
dolzycka <dolzyckalian@gmail.com>
2026-01-14 16:39:06 +0100

    fix(mail): added mocking to test concerning the sending of emails and vectorizing

--- src/app/api/employee/route.test.ts
+++ src/app/api/employee/route.test.ts
@@ -1,3 +1,19 @@
+// Prepare mocking for sending emails and vectorizing - must be defined before importing the route handlers
+import { jest } from '@jest/globals';
+
+jest.unstable_mockModule('@/app/api/email/mailer', () => ({
+  sendEmail: jest.fn(),
+}));
+
+jest.unstable_mockModule('src/services/server/vectorizer.ts', () => ({
+  vectorizeExpertiseArea: jest.fn(async () => {
+    const arr = Array(3072).fill(0.01);
+    return `[${arr.join(',')}]`;
+  }),
+}));
+
+// Non-mock related implementation:
+
 import { AccountType, Area, Gender, Language, Pronoun } from '~/generated/prisma/enums';
 import type { OrganizationCreateInput } from '~/generated/prisma/models';
 
