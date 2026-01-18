9be89e247ac9d96e5cecc5b976a764db5d16c897
Max Uden <max.uden@t-online.de>
2026-01-18 21:23:04 +0100

    refactor(authorization): added helper function to check if employee.orgID is the same as given orgID in route

--- src/app/api/organization/[organizationID]/services/[serviceID]/route.ts
+++ src/app/api/organization/[organizationID]/services/[serviceID]/route.ts
@@ -6,11 +6,13 @@ import { verifyJWT } from '@/app/api/authentication/login/JWTService';
 import { handleValidationError, validateHeader } from '@/app/api/helper';
 import { PricingModel, ServiceType } from '~/generated/prisma/enums';
 
+import { isOrganizationEmployeeMatch } from '../helpers';
 import { deleteService } from './services';
 import { updateService } from './services';
 
 const paramsSchema = z.strictObject({
-  serviceID: z.uuid({ error: 'Case ID is required' }),
+  serviceID: z.uuid({ error: 'Service ID is required' }),
+  organizationID: z.uuid({ error: 'Organization ID is required' }),
 });
 const serviceUpdateSchema = z.strictObject({
   title: z.string('title is required').optional(),
@@ -25,16 +27,19 @@ const serviceUpdateSchema = z.strictObject({
 // Update a service
 export async function PATCH(
   req: NextRequest,
-  { params }: { params: Promise<{ serviceID: string }> }
+  { params }: { params: Promise<{ serviceID: string; organizationID: string }> }
 ) {
-  // verify employee is logged in
-  const jwtString = req.cookies.get('access_token')?.value;
-  const loginRes = verifyJWT(jwtString);
-  if (loginRes.employeeId) {
-    try {
-      // validate URL Param
-      const { serviceID } = await params;
-      paramsSchema.parse({ serviceID });
+  try {
+    // validate URL Param
+    const { serviceID, organizationID } = await params;
+    paramsSchema.parse({ serviceID, organizationID });
+    // verify employee is logged in
+    const jwtString = req.cookies.get('access_token')?.value;
+    const loginRes = verifyJWT(jwtString);
+    if (
+      loginRes.employeeId &&
+      (await isOrganizationEmployeeMatch(organizationID, loginRes.employeeId))
+    ) {
       // validate header
       validateHeader(req.headers);
       // validate body
@@ -43,19 +48,19 @@ export async function PATCH(
       // handle update/patch
       const updatedCase = await updateService(serviceID, body);
       return NextResponse.json(updatedCase, { status: 200 });
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return handleValidationError(error);
-      } else {
-        return NextResponse.json(
-          { message: 'Update failed: ' + (error as Error).message },
-          { status: 400 }
-        );
-      }
+    } else {
+      // unauthorized
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
-  } else {
-    // unauthorized
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return handleValidationError(error);
+    } else {
+      return NextResponse.json(
+        { message: 'Update failed: ' + (error as Error).message },
+        { status: 400 }
+      );
+    }
   }
 }
 
@@ -63,27 +68,28 @@ export async function PATCH(
 // Delete a service
 export async function DELETE(
   _req: NextRequest,
-  { params }: { params: Promise<{ serviceID: string }> }
+  { params }: { params: Promise<{ serviceID: string; organizationID: string }> }
 ) {
-  // verify employee is logged in
-  const jwtString = _req.cookies.get('access_token')?.value;
-  const loginRes = verifyJWT(jwtString);
-  if (loginRes.employeeId) {
-    try {
-      const { serviceID } = await params;
-      if (!serviceID) {
-        return NextResponse.json({ message: 'Case ID is required' }, { status: 400 });
-      }
+  try {
+    const { serviceID, organizationID } = await params;
+    paramsSchema.parse({ serviceID, organizationID });
+    // verify employee is logged in
+    const jwtString = _req.cookies.get('access_token')?.value;
+    const loginRes = verifyJWT(jwtString);
+    if (
+      loginRes.employeeId &&
+      (await isOrganizationEmployeeMatch(organizationID, loginRes.employeeId))
+    ) {
       await deleteService(serviceID);
       return NextResponse.json({ status: 204 });
-    } catch (error) {
-      return NextResponse.json(
-        { message: 'Failed to delete organization: ' + (error as Error).message },
-        { status: 400 }
-      );
+    } else {
+      // unauthorized
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
-  } else {
-    // unauthorized
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: 'Failed to delete organization: ' + (error as Error).message },
+      { status: 400 }
+    );
   }
 }
--- src/app/api/organization/[organizationID]/services/route.ts
+++ src/app/api/organization/[organizationID]/services/route.ts
@@ -1,12 +1,14 @@
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
-
-import { createService, readServices } from './services';
 import { z } from 'zod';
-import { handleValidationError, validateHeader } from '@/app/api/helper';
-import { ServiceCreateInput } from '~/generated/prisma/models';
-import { PricingModel, ServiceType } from '~/generated/prisma/enums';
+
 import { verifyJWT } from '@/app/api/authentication/login/JWTService';
+import { handleValidationError, validateHeader } from '@/app/api/helper';
+import { PricingModel, ServiceType } from '~/generated/prisma/enums';
+import type { ServiceCreateInput } from '~/generated/prisma/models';
+
+import { isOrganizationEmployeeMatch } from './helpers';
+import { createService, readServices } from './services';
 
 // GET /api/organization/:organizationID/services
 export async function GET(
@@ -39,22 +41,25 @@ const paramsSchema = z.strictObject({
   organizationID: z.uuid({ error: 'Organization ID is required' }),
 });
 
-// POST /api/case/
+// POST /api/organization/:organizationID/services
 // Create a new case manually without any appointments
 export async function POST(
   req: NextRequest,
   { params }: { params: Promise<{ organizationID: string }> }
 ) {
-  // verify employee is logged in
-  const jwtString = req.cookies.get('access_token')?.value;
-  const loginRes = verifyJWT(jwtString);
-  if (loginRes.employeeId) {
-    try {
-      // validate header
-      validateHeader(req.headers);
-      // validate params
-      const { organizationID } = await params;
-      paramsSchema.parse({ organizationID });
+  try {
+    // verify employee is logged in
+    const jwtString = req.cookies.get('access_token')?.value;
+    const loginRes = verifyJWT(jwtString);
+    // validate header
+    validateHeader(req.headers);
+    // validate params
+    const { organizationID } = await params;
+    paramsSchema.parse({ organizationID });
+    if (
+      loginRes.employeeId &&
+      (await isOrganizationEmployeeMatch(organizationID, loginRes.employeeId))
+    ) {
       // validate body
       const body = serviceCreateSchema.parse(await req.json());
       // links to existing Organization
@@ -71,18 +76,18 @@ export async function POST(
 
       const createdAppointment = await createService(serviceInput);
       return NextResponse.json(createdAppointment, { status: 201 });
-    } catch (error) {
-      if (error instanceof z.ZodError) {
-        return handleValidationError(error);
-      } else {
-        return NextResponse.json(
-          { message: 'Creation failed: ' + (error as Error).message },
-          { status: 400 }
-        );
-      }
+    } else {
+      // unauthorized
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
     }
-  } else {
-    // unauthorized
-    return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return handleValidationError(error);
+    } else {
+      return NextResponse.json(
+        { message: 'Creation failed: ' + (error as Error).message },
+        { status: 400 }
+      );
+    }
   }
 }
