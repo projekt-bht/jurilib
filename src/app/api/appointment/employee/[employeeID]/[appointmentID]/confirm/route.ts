cc72acb269995ab35d5aa73efe868b3c05fb5505
Max Uden <max.uden@t-online.de>
2026-01-18 16:46:35 +0100

    feat(appointment): added /confirm endpoint for employee interaction

---
+++ src/app/api/appointment/employee/[employeeID]/[appointmentID]/confirm/route.ts
@@ -0,0 +1,51 @@
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+import { z } from 'zod';
+
+import { verifyJWT } from '@/app/api/authentication/login/JWTService';
+import { handleValidationError } from '@/app/api/helper';
+
+import { confirmAppointmentCreateCase } from './services';
+
+/**
+ * Validate parameter appointmentId as uuid
+ */
+// const paramsSchema = z.object({
+const paramsSchema = z.strictObject({
+  appointmentID: z.uuid({ error: 'AppointmentId is required' }),
+  employeeID: z.uuid({ error: 'EmployeeId is required' }),
+});
+
+// POST /api/appointment/employee/:employeeId/:appointmentId/confirm
+// Booking Endpoint for employee interaction. Requires authentication. Sets status to "CONFIRMED". Creates Case with given appointment
+export async function POST(
+  _req: NextRequest,
+  { params }: { params: Promise<{ employeeID: string; appointmentID: string }> }
+) {
+  try {
+    // get appointmentId from URL params
+    const { employeeID, appointmentID } = await params;
+    paramsSchema.parse({ appointmentID, employeeID });
+
+    // verify user is logged in
+    const jwtString = _req.cookies.get('access_token')?.value;
+    const loginRes = verifyJWT(jwtString);
+    if (loginRes.employeeId) {
+      // book apppointment
+      const bookedAppointment = await confirmAppointmentCreateCase(appointmentID, employeeID);
+      return NextResponse.json(bookedAppointment, { status: 200 });
+    } else {
+      // unauthorized
+      return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+    }
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return handleValidationError(error);
+    } else {
+      return NextResponse.json(
+        { message: 'Update failed: ' + (error as Error).message },
+        { status: 400 }
+      );
+    }
+  }
+}
---
+++ src/app/api/appointment/employee/[employeeID]/[appointmentID]/confirm/services.ts
@@ -0,0 +1,57 @@
+import {
+  type CaseCreateWithAppointmentInput,
+  createCaseWithAppointment,
+} from '@/app/api/case/services';
+import prisma from '@/lib/db';
+import type { Case } from '~/generated/prisma/client';
+import { AppointmentStatus } from '~/generated/prisma/client';
+
+// Book an appointment by Id
+export async function confirmAppointmentCreateCase(
+  appointmentId: string,
+  employeeId: string
+): Promise<Case> {
+  try {
+    // check if appointment already has a userId
+    const appointment = await prisma.appointment.findUnique({
+      where: { id: appointmentId },
+    });
+    // only continue, if userId is given!
+    if (appointment?.userId) {
+      // confirm appointment
+      await prisma.appointment.update({
+        where: { id: appointmentId },
+        data: {
+          status: AppointmentStatus.CONFIRMED,
+        },
+      });
+
+      // get user information to
+      const user = await prisma.user.findUnique({
+        where: { id: appointment.userId },
+      });
+      if (!user) {
+        throw new Error('User not found');
+      }
+      const { firstname, lastname } = user;
+
+      const caseCreationData: CaseCreateWithAppointmentInput = {
+        title: `Fall zu ${firstname} ${lastname}`,
+        description: `Neuer Fall vom ${new Date().toLocaleDateString()}`,
+        employee: { connect: { id: employeeId } },
+        appointmentId: appointmentId,
+      };
+      const createdCase = await createCaseWithAppointment(caseCreationData);
+      const returnCase = await prisma.case.findUnique({
+        where: { id: createdCase?.id },
+      });
+      if (!returnCase) {
+        throw new Error('Case not found');
+      }
+      return returnCase;
+    }
+    throw new Error('Appointment is not assigned to any user');
+  } catch (error) {
+    throw new Error('Database update failed: ' + (error as Error).message);
+  }
+}
