c3e1877c5dfdce3474294b87e6080a03ae122908
dolzycka <dolzyckalian@gmail.com>
2026-01-19 17:10:02 +0100

    feat(zod): added new helper function to validate ids and optimized handleValidationError

--- src/app/api/helper.ts
+++ src/app/api/helper.ts
@@ -3,24 +3,43 @@
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
 
-/**
- * Validate the 'content-type' of the request header is 'application/json'
- */
+// ---------- header validation ----------
+
+// Validate the 'content-type' of the request header is 'application/json'
 const headerSchema = z.object({
   'content-type': z.string().includes('application/json', {
     message: 'Invalid content type, must be application/json',
   }),
 });
 
-/**
- * Validate request headers, specifically the content-type
- */
+// Validate request headers, specifically the content-type
 export function validateHeader(headers: Headers): void {
   const contentType = headers.get('content-type');
   headerSchema.parse({ 'content-type': contentType ?? '' });
 }
 
+// ---------- param id validation ----------
+/**
+ * Validate that given params contain valid UUIDs
+ * @param params id is the value to validate, identifier is a descriptive name for error messages
+ */
+export function validateId(params: [{ id: string; identifier: string }]): void {
+  for (const param of params) {
+    const idSchema = z.strictObject({
+      id: z.uuid({ message: 'Invalid UUID format for ' + param.identifier }),
+    });
+    idSchema.parse(param.id);
+  }
+}
+
+// ---------- error handling ----------
+
 export function handleValidationError(error: z.ZodError) {
   console.error('Validation error:', error);
-  return NextResponse.json({ message: 'Validation error', errors: error }, { status: 400 });
+  let errorStack = {};
+  for (const issue of error.issues) {
+    const message = issue.message;
+    errorStack = { ...errorStack, message };
+  }
+  return NextResponse.json({ message: 'Validation error', errors: errorStack }, { status: 400 });
 }
