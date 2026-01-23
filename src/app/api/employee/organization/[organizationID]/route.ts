2c504abb012c5607cd051c536a40a172a9a7533c
dolzycka <dolzyckalian@gmail.com>
2026-01-23 17:36:41 +0100

    feat(zod): route checked

--- src/app/api/employee/organization/[organizationID]/route.ts
+++ src/app/api/employee/organization/[organizationID]/route.ts
@@ -1,7 +1,8 @@
-// TODO: check ZOD validation
-
 import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
+import * as z from 'zod';
+
+import { handleValidationError, validateIds } from '@/app/api/helper';
 
 import { readEmployeesByOrganizationID } from './services';
 
@@ -13,13 +14,14 @@ export async function GET(
 ) {
   try {
     const { organizationID } = await params;
-    if (!organizationID) {
-      return NextResponse.json({ message: 'Organization ID is required' }, { status: 400 });
-    }
+    validateIds([{ id: organizationID, identifier: 'organizationID' }]);
 
     const employees = await readEmployeesByOrganizationID(organizationID);
     return NextResponse.json(employees, { status: 200 });
   } catch (error) {
+    if (error instanceof z.ZodError) {
+      return handleValidationError(error);
+    }
     return NextResponse.json({ message: (error as Error).message }, { status: 404 });
   }
 }
