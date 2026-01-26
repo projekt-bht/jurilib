66a224b6a33c507c17fe194f8318e484c80923ab
s87622 <s87622@bht-berlin.de>
2026-01-26 19:57:01 +0100

    feat(dashboard): show Userdashoard if there is a LoginContext

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,20 @@
-import { LandingPage } from '@/components/LandingPage/LandingPage';
+'use client';
+import { UserDashboard } from '@/components/dashboard/_components/user/UserDashboard';
+import { LandingPage } from '@/components/landingPage/LandingPage';
+
+import { useLoginContext } from './LoginContext';
 
 export default function Home() {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const { login, setLogin } = useLoginContext();
+
+  if (login) {
+    return (
+      <div className="min-h-screen bg-background">
+        <UserDashboard />
+      </div>
+    );
+  }
+
   return <LandingPage />;
 }
