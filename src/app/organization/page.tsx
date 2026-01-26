65a534c8b2c6905f8da0243f40ac022df4de9128
s87619 <s87619@bht-berlin.de>
2026-01-26 20:59:41 +0100

    fix: add comment to indicate pagination adjustment needed

--- src/app/organization/page.tsx
+++ src/app/organization/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+//Pagination von Max muss angepasst werden
 // import Link from 'next/link';
 //
 // import { OrganizationCard } from '@/app/organization/_components/OrganizationCard';
