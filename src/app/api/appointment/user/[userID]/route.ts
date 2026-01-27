65f3cf5ccac6aa71eb4ce9fe0449463ac3d878de
Max Uden <max.uden@t-online.de>
2026-01-27 18:33:08 +0100

    feat(appointment): use auth handler to secure user appointments

--- src/app/api/appointment/user/[userID]/route.ts
+++ src/app/api/appointment/user/[userID]/route.ts
@@ -2,29 +2,42 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import { z } from 'zod';
 
-import { handleValidationError } from '../../../helper';
+import { withUserAuth } from '@/lib/withAuth';
+import type { UserLoginResource } from '@/services/Resources';
+
+import { handleValidationError, unauthorized } from '../../../helper';
 import { readAllAppointmentsByUser } from './service';
 
 // GET api/appointment/:userID
 // Retrieve all appointments of user
-export async function GET(req: NextRequest, { params }: { params: Promise<{ userID: string }> }) {
-  try {
-    // validate userID
-    const { userID } = await params;
-    paramsSchema.parse({ userID });
-    const appointments = await readAllAppointmentsByUser(userID);
-    return NextResponse.json(appointments, { status: 200 });
-  } catch (error) {
-    if (error instanceof z.ZodError) {
-      return handleValidationError(error);
-    } else {
-      return NextResponse.json(
-        { message: 'Read failed: ' + (error as Error).message },
-        { status: 400 }
-      );
+export const GET = withUserAuth(
+  async (
+    _req: NextRequest,
+    { params }: { params: Promise<{ userID: string }> },
+    account: UserLoginResource
+  ) => {
+    try {
+      // validate userID
+      const { userID } = await params;
+      paramsSchema.parse({ userID });
+
+      // check if loginResource and userid given by url-param are the same
+      if (!(userID === account.userId)) return unauthorized();
+
+      const appointments = await readAllAppointmentsByUser(account.userId);
+      return NextResponse.json(appointments, { status: 200 });
+    } catch (error) {
+      if (error instanceof z.ZodError) {
+        return handleValidationError(error);
+      } else {
+        return NextResponse.json(
+          { message: 'Read failed: ' + (error as Error).message },
+          { status: 400 }
+        );
+      }
     }
   }
-}
+);
 
 /**
  * Validate parameter userID
