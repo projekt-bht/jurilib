9ccef811a5d853db5da802e6a1cb04ba84a6306b
dolzycka <dolzyckalian@gmail.com>
2026-01-28 17:48:34 +0100

    feat(error): added new possible error "immutableField"

--- src/error/validationErrors.ts
+++ src/error/validationErrors.ts
@@ -8,12 +8,14 @@ export class ValidationError extends Error {
   constructor(
     public details:
       | 'duplicate'
-      | 'notFound'
+      | 'immutableField'
+      | 'invalidContentType'
       | 'invalidInput'
-      | 'mismatch'
       | 'invalidReference'
-      | 'overlappingAppointment'
-      | 'invalidContentType',
+      | 'mismatch'
+      | 'notFound'
+      | 'overlappingAppointment',
+
     public field?: string,
     public value?: unknown,
     public statusCode: number = 400
@@ -25,18 +27,20 @@ export class ValidationError extends Error {
     switch (this.details) {
       case 'duplicate':
         return 'There is already an entry with these attributes.';
-      case 'notFound':
-        return 'The entry could not be found.';
-      case 'invalidInput':
-        return 'The given input is invalid.';
-      case 'mismatch':
-        return 'The given ID in the parameter does not match the ID in the body.';
-      case 'invalidReference':
-        return 'The reference is invalid.';
-      case 'overlappingAppointment':
-        return 'The appointment overlaps with an existing appointment.';
+      case 'immutableField':
+        return 'The field is immutable and cannot be changed.';
       case 'invalidContentType':
         return 'Invalid content type, must be application/json';
+      case 'invalidInput':
+        return 'The given input is invalid.';
+      case 'invalidReference':
+        return 'The reference is invalid.';
+      case 'mismatch':
+        return 'The given ID in the parameter does not match the ID in the body.';
+      case 'notFound':
+        return 'The entry could not be found.';
+      case 'overlappingAppointment':
+        return 'The appointment overlaps with an existing appointment.';
     }
   }
 }
