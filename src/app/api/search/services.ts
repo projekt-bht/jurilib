b69d4e90923f4e2fb5c1ac67ba5471caa5669591
parker <141875371+oz28@users.noreply.github.com>
2026-01-30 18:06:02 +0100

    fix(search): changed threshold

--- src/app/api/search/services.ts
+++ src/app/api/search/services.ts
@@ -9,7 +9,7 @@ import type {
 import type { Organization } from '~/generated/prisma/client';
 
 const similarityOffset = 0.8;
-const threshold = 0.1;
+const threshold = 0.05;
 
 export async function createSearch(query: string) {
   const searchInput = await vectorizeSearch(query);
