c19a89910245d1dc74e42249783c6236732525e6
s87622 <s87622@bht-berlin.de>
2026-01-31 16:34:11 +0100

    fix(import): fix importstatement

--- src/app/organization/[organizationID]/loading.tsx
+++ src/app/organization/[organizationID]/loading.tsx
@@ -1,4 +1,4 @@
-import LoadingComponent from '@/components/Loading/LoadingComponent';
+import LoadingComponent from '@/components/loading/LoadingComponent';
 
 export default function Loading() {
   return <LoadingComponent message="Die Organisation wird für dich geladen..." />;
