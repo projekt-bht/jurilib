bdc1566102f95bc88f0292a492cabc8c3df7efeb
parker <141875371+oz28@users.noreply.github.com>
2026-02-02 22:37:30 +0100

    test(vectorizer): createScenario template method

--- src/services/server/vectorizer.test.ts
+++ src/services/server/vectorizer.test.ts
@@ -4,16 +4,23 @@ import { Area } from '~/generated/prisma/enums';
 
 import { createEmbedding, vectorizeSearch } from './vectorizer';
 
+async function createScenarioTemplate(userInput: string): Promise<boolean> {
+  const userSearch = userInput;
+  const searchResult = await vectorizeSearch(userSearch);
+  const embeddingResult = await createEmbedding([Area.Verkehrsrecht].toString());
+  const similarityResult = similarity(
+    JSON.parse(searchResult.area) as number[],
+    JSON.parse(embeddingResult) as number[]
+  );
+
+  return similarityResult ? similarityResult > 0.99 : false;
+}
+
 describe('Vectorizer Golden Samples', () => {
   test('Basic Verkehrsrecht', async () => {
-    const userSearch =
-      'Ich hatte einen Auffahrunfall und möchte wissen, wer haftet und welche Schritte ich rechtlich einleiten kann.';
-    const searchResult = await vectorizeSearch(userSearch);
-    const embeddingResult = await createEmbedding([Area.Verkehrsrecht].toString());
-    const similarityResult = similarity(
-      JSON.parse(searchResult.area) as number[],
-      JSON.parse(embeddingResult) as number[]
+    const isScenarioCorrect = await createScenarioTemplate(
+      'Ich hatte einen Auffahrunfall und möchte wissen, wer haftet und welche Schritte ich rechtlich einleiten kann.'
     );
-    expect(similarityResult).toBeGreaterThan(0.99);
+    expect(isScenarioCorrect).toBeTruthy();
   });
 });
