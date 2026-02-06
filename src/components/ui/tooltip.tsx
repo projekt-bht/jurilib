808e4f0ca55dfd00cd3a888cd04409386880b7a9
s87619 <s87619@bht-berlin.de>
2026-02-06 18:49:00 +0100

    fix(ui): update imports and improve component structure in button, dialog, input, and tooltip

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as React from "react"
-import { Tooltip as TooltipPrimitive } from "radix-ui"
+import * as TooltipPrimitive from "@radix-ui/react-tooltip"
 
 import { cn } from "@/lib/utils"
 
@@ -21,7 +21,11 @@ function TooltipProvider({
 function Tooltip({
   ...props
 }: React.ComponentProps<typeof TooltipPrimitive.Root>) {
-  return <TooltipPrimitive.Root data-slot="tooltip" {...props} />
+  return (
+    <TooltipProvider>
+      <TooltipPrimitive.Root data-slot="tooltip" {...props} />
+    </TooltipProvider>
+  )
 }
 
 function TooltipTrigger({
