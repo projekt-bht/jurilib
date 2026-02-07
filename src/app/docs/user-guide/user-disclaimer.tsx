62b23ceef4200a84ea753f57002d3703d2aa1708
dolzycka <dolzyckalian@gmail.com>
2026-02-07 16:45:50 +0100

    feat(docs-page): updates disclaimer

--- src/app/docs/user-guide/user-disclaimer.tsx
+++ src/app/docs/user-guide/user-disclaimer.tsx
@@ -1,4 +1,4 @@
-import { Ban, TriangleAlert } from 'lucide-react';
+import { Ban, Loader, SquareCheckBig, TriangleAlert } from 'lucide-react';
 
 export function Disclaimer() {
   return (
@@ -9,12 +9,23 @@ export function Disclaimer() {
       </h3>
       <p className="text-m text-foreground leading-relaxed">
         Im folgenden wird an einer Stelle ein Feature erwähnt, dass im Backend schon implementiert
-        ist, aber noch nicht durch das frontend nutzbar ist.
+        größtenteils implementiert, aber noch nicht durch das frontend nutzbar ist.
       </p>
       <br />
       <p className="text-m text-foreground leading-relaxed">
-        Um dieses Feature zu kennzeicheichnen, haben wir es mit folgendem Label versehen:
+        Um dieses Feature zu kennzeicheichnen und die Teilfeatures zu kennzeichnen, haben wir sie
+        mit folgendem Labels versehen:
       </p>
+      <div className="flex gap-2 text-sm text-primary items-center">
+        <SquareCheckBig className="w-4 h-4 text-primary shrink-0 m-1" />
+        Diese Funktion ist im Backend bereits vollständig implementiert
+      </div>
+
+      <div className="flex gap-2 text-sm text-primary items-center">
+        <Loader className="w-4 h-4 text-primary shrink-0 m-1" />
+        Diese Funktion ist im Backend bereits teilweise implementiert
+      </div>
+
       <div className="flex gap-2 text-sm text-primary items-center">
         <Ban className="w-4 h-4 text-primary shrink-0 m-1" />
         Diese Funktion ist noch nicht implementiert, aber in Planung
